92310cd9a0bc735ce54d45799092afddc51bcc94
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2019-10-24 16:31:02 +0200

    static mess

--- geomapshark/settings.py
+++ geomapshark/settings.py
@@ -174,7 +174,6 @@ LOCALE_PATHS = (
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
