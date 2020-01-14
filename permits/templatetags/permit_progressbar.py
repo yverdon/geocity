2f1c335aa2b50e5c65e4adeae7f1707646010565
sit <olivier.monod@yverdon-les-bains.ch>
2020-01-14 12:54:02 +0100

    progressbar wip

--- permits/templatetags/permit_progressbar.py
+++ permits/templatetags/permit_progressbar.py
@@ -10,12 +10,11 @@ register = template.Library()
 def permit_progressbar(permit_request, step):
 
 
-    steps_states = {
-        'current_step': step,
-        'WorksTypesForm': False, # must have one item selected a least
-        'WorksObjectsForm': False, # must have one object by worktype selected at least
-        'WorksObjectsPropertiesForm': False, # must be valid
-        'WorksObjectsAppendicesForm': False, # must be valid
+    steps = {
+        'WorksTypesForm': {'state': 'done', 'name': 'Types', 'number': 1, 'current': step}, # must have one item selected a least
+        'WorksObjectsForm': {'state':'partial', 'name': 'Objets', 'number': 2 ,'current': step}, # must have one object by worktype selected at least
+        'WorksObjectsPropertiesForm': {'state': 'partial', 'name': 'Détails', 'number': 3,'current': step}, # must be valid
+        'WorksObjectsAppendicesForm': {'state': 'todo', 'name': 'Documents', 'number': 4,'current': step}, # must be valid
     }
 
     if permit_request:
@@ -23,13 +22,15 @@ def permit_progressbar(permit_request, step):
         # check validation state for properties
         form = forms.WorksObjectsPropertiesForm(instance=permit_request, enable_validation=True)
         if form.is_valid():
-            step_states['WorksObjectsPropertiesForm'] = True
+            steps['WorksObjectsPropertiesForm'] = True
         print(form.errors)
         # check validation state for appendices
         form = forms.WorksObjectsAppendicesForm(instance=permit_request, enable_validation=True)
         if form.is_valid():
-            step_states['WorksObjectsAppendicesForm'] = True
+            steps['WorksObjectsAppendicesForm'] = True
 
-
+    steps_states = {
+        'steps': steps
+    }
 
     return steps_states
