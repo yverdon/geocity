3512de610d5a1233ada57829b3fd4001ee348d79
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2020-03-04 12:08:02 +0100

    implement ygdp4

---
+++ permits/tables.py
@@ -0,0 +1,27 @@
+import django_tables2 as tables
+from django.utils.html import format_html
+from . import models
+
+
+class PermitRequestTableExterns(tables.Table):
+
+    edit_properties = tables.TemplateColumn('<a title="Modifier" href="{% url \'permits:permit_request_properties\' record.id %}"> \
+     <i class="fa fa-edit fa-lg permit_status_{{record.status}}"></i></a>', verbose_name='Modifier', orderable=False)
+
+    submit = tables.TemplateColumn('<a title="Modifier" href="{% url \'permits:permit_request_submit\' record.id %}"> \
+    <i class="fa fa-envelope fa-lg permit_status_{{record.status}}"></i></a>', verbose_name='Envoyer', orderable=False)
+
+    delete = tables.TemplateColumn('<a title="Supprimer la demande" href="{% url \'permits:permit_request_delete\' record.id %}"> \
+    <i class="fa fa-trash fa-lg"></i></a>', verbose_name='Supprimer', orderable=False)
+
+
+    class Meta:
+        model = models.PermitRequest
+        fields = ('id', 'status', 'created_at', 'administrative_entity')
+        template_name = 'django_tables2/bootstrap.html'
+
+class PermitExportTable(tables.Table):
+
+    class Meta:
+        model = models.PermitRequest
+        template_name = 'django_tables2/bootstrap.html'
