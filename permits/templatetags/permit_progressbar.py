63b7b1d120cc5329a6b1119a22c6eaa9de956dd8
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2020-04-07 16:15:38 +0200

    YGDP-3 use errors count in progress bar

--- permits/templatetags/permit_progressbar.py
+++ permits/templatetags/permit_progressbar.py
@@ -1,12 +1,10 @@
 import dataclasses
-from typing import Dict, List
 
 from django import template
-from django.forms import modelformset_factory
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
-from permits import forms, models, services
+from permits import forms, services
 
 register = template.Library()
 
@@ -17,7 +15,7 @@ class Step:
     url: str
     completed: bool = False
     enabled: bool = False
-    errors: List[Dict[str, str]] = dataclasses.field(default_factory=list)
+    errors_count: int = 0
 
 
 @register.inclusion_tag('permits/_permit_progressbar.html', takes_context=True)
@@ -56,7 +54,10 @@ def permit_progressbar(context, permit_request, active_step):
         instance=permit_request, enable_required=True, disable_fields=True, data={}
     ) if permit_request else None
 
-    actor_errors = services.get_missing_actors_types(permit_request)
+    properties_errors = len(properties_form.errors) if properties_form else 0
+    appendices_errors = len(appendices_form.errors) if appendices_form else 0
+    actor_errors = len(services.get_missing_actors_types(permit_request)) if permit_request else 0
+    total_errors = sum([properties_errors, appendices_errors, actor_errors])
 
     steps = {
         "location": Step(
@@ -81,29 +82,29 @@ def permit_progressbar(context, permit_request, active_step):
             name=_("Détails"),
             url=properties_url,
             completed=has_objects_types and properties_form and not properties_form.errors,
-            errors=properties_form.errors if properties_form else [],
+            errors_count=properties_errors,
             enabled=has_objects_types,
         ),
         "appendices": Step(
             name=_("Documents"),
             url=appendices_url,
             completed=has_objects_types and appendices_form and not appendices_form.errors,
-            errors=appendices_form.errors if appendices_form else [],
+            errors_count=appendices_errors,
             enabled=has_objects_types,
         ),
         "actors": Step(
             name=_("Contacts"),
             url=actors_url,
             enabled=has_objects_types,
-            errors=actor_errors,
+            errors_count=actor_errors,
             completed=not actor_errors,
         ),
         "submit": Step(
             name=_("Résumé et envoi"),
             url=submit_url,
             enabled=has_objects_types,
-            errors={**appendices_form.errors, **properties_form.errors, **dict.fromkeys(actor_errors, 1)},
-            completed=len({**appendices_form.errors, **properties_form.errors, **dict.fromkeys(actor_errors, 1)})==0,
+            errors_count=total_errors,
+            completed=total_errors == 0,
         ),
     }
     steps_states = {
