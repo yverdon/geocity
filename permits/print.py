747a35da61b25ab4886994df8a50c7a6508c2662
Monod Olivier <OLM@ylb.ch>
2020-05-18 14:44:33 +0200

    basic print

--- permits/print.py
+++ permits/print.py
@@ -5,7 +5,7 @@ from django.conf import settings
 import urllib.parse
 import urllib.request
 from uuid import uuid4
-from . import services, models
+from . import services, models, forms
 
 
 def get_permit_request_for_edition(user, permit_request_id):
@@ -25,10 +25,40 @@ def printreport(request, permit_request_id):
 
     print_date = datetime.datetime.now()
 
+    properties_form = forms.WorksObjectsPropertiesForm(instance=permit_request)
+    properties_by_object_type = dict(properties_form.get_fields_by_object_type())
+    appendices_form = forms.WorksObjectsAppendicesForm(instance=permit_request)
+    appendices_by_object_type = dict(appendices_form.get_fields_by_object_type())
+
+    objects_infos = [
+        (
+            obj,
+            properties_by_object_type.get(obj, []),
+            appendices_by_object_type.get(obj, [])
+        )
+        for obj in permit_request.works_object_types.all()
+    ]
+
+    actor_types = dict(models.ACTOR_TYPE_CHOICES)
+
+    contacts = [
+        (actor_types.get(contact['actor_type'].value(), ''), [
+            (field.label, field.value())
+            for field in contact
+            if field.name not in {'id', 'actor_type'}
+        ])
+        for contact in services.get_permitactorformset_initiated(permit_request)
+        if contact['id'].value()
+    ]
+
+
     html = render(request, "permits/print/printpermit.html", {
         'permit_request': permit_request,
+        'contacts': contacts,
+        'objects_infos': objects_infos,
     })
 
-    pdf_file = HTML(string=html.content,  base_url=request.build_absolute_uri()).write_pdf()
+    pdf_file = HTML(string=html.content,  base_url=request.build_absolute_uri()).write_pdf(
+        stylesheets=[CSS('/code/static/css/printpermit.css')]) #FIX THAT
     file_path = 'tutu'
     return pdf_file, file_path
