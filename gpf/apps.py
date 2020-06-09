96abc36155425950e6df59880731999c9781dd9f
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2020-06-09 12:39:11 +0200

    remove gpf app directory

--- gpf/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GpfConfig(AppConfig):
-    name = 'gpf'
--- gpf/widgets.py
+++
@@ -1,5 +0,0 @@
-from django.forms import widgets
-
-class RemoteAutocompleteWidget(widgets.TextInput):
-    template_name = 'remoteautocomplete/remoteautocomplete.html'
-    pass
