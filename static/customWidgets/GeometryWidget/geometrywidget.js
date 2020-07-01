a6fd1c527338dd4ed404e945df226c79ba2bdc8f
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2020-07-01 08:41:29 +0200

    revert

--- static/customWidgets/GeometryWidget/geometrywidget.js
+++ static/customWidgets/GeometryWidget/geometrywidget.js
@@ -395,25 +395,7 @@
         // Initialize the modify interaction
         this.interactions.modify = new ol.interaction.Modify({
             source: this.vectorSource,
-            style: new ol.style.Style({}),
-            condition: (e) => {
-              $('#out-of-administrative-limits').hide();
-              let coords = e.coordinate;
-              let features = this.map.getFeaturesAtPixel(e.pixel, {
-                layerFilter: (layer) => {
-                  return layer === this.vectorMaskLayer;
-                }
-              });
-              if (features && features.length > 0) {
-                return true;
-              } else {
-                  $('#out-of-administrative-limits').show();
-                  $('#out-of-administrative-limits').html(
-                    "Votre saisie sort du territoire du territoire concerné"
-                    )
-                return false;
-              }
-            }
+            style: new ol.style.Style({})
         });
 
 
