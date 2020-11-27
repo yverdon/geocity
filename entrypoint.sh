d2bc541b6147cb2b0cbada38605034c361c7ee94
Monod Olivier <OLM@ylb.ch>
2020-11-27 11:01:58 +0100

    use django constance and fix fixturize

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 cd /code
 # setup app using the django tools
 python3 manage.py migrate
+# django-constance models
+python3 manage.py migrate database
+
 mkdir /code/geomapshark/static/
 echo yes | python3 manage.py compilemessages -l fr
 echo yes | python3 manage.py collectstatic
