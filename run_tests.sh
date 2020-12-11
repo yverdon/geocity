681c64d45ac61ff82755dbf5a8a542219ee3056f
loicgasser <loicgasser4@gmail.com>
2020-12-10 20:20:15 -0500

    Backup and restore env file

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Save env variables
+if [ -f .env ]; then
+  cp -f .env env.backup
+fi
 cp -f env.demo .env
+
 # test with 2FA
 coverage run --source='.' ./manage.py test --settings=geomapshark.settings_test
 coverage report -m
@@ -12,3 +17,9 @@ coverage report -m
 
 # put it back to true
 sed -i -e 's/ENABLE_2FA\=false/ENABLE_2FA\=true/g' .env\
+
+# Restore env variables
+if [ -f env.backup ]; then
+  cp -f env.backup .env
+  rm env.backup
+fi
