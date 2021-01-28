2bcd544b4b0d6ab2f1c7e15b20edb0383dc25a2f
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2021-01-28 15:22:24 +0100

    YC-159 remove unused class PlaceholderInput

--- permits/forms.py
+++ permits/forms.py
@@ -19,17 +19,6 @@ from django.utils.translation import gettext_lazy as _
 from . import models, services
 
 
-class PlaceholderInput(forms.widgets.Input):
-    template_name = "about/placeholder.html"
-    input_type = "text"
-
-    def get_context(self, name, value, attrs):
-        context = super(PlaceholderInput, self).get_context(name, value, attrs)
-        context["widget"]["attrs"]["maxlength"] = 50
-        context["widget"]["attrs"]["placeholder"] = name.title()
-        return context
-
-
 class AddressWidget(forms.widgets.TextInput):
     @property
     def media(self):
