48385fb063ea50b5bc5b5e15ed32962ab460e610
Felice Candilio <felicemcc@gmail.com>
2021-02-09 10:06:09 +0100

    YC-62 Showing information of responsible service in summary

--- permits/templatetags/permits_extras.py
+++ permits/templatetags/permits_extras.py
@@ -60,6 +60,9 @@ def permit_request_summary(context, permit_request):
         "intersected_geometries": permit_request.intersected_geometries
         if permit_request.intersected_geometries != ""
         else None,
+        "amend_custom_properties_values": services.get_amend_custom_properties_values_by_object_type(
+            permit_request
+        ),
     }
 
 
