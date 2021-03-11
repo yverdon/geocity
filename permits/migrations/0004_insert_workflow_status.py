8246179c6d862402c980d6a00d03822896a92385
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-03-11 11:30:33 +0100

    remove call to model in migration

--- permits/migrations/0004_insert_workflow_status.py
+++ permits/migrations/0004_insert_workflow_status.py
@@ -2,18 +2,42 @@
 
 from django.db import migrations
 
-from permits import models as permitmodels
+
+STATUS_DRAFT = 0
+STATUS_SUBMITTED_FOR_VALIDATION = 1
+STATUS_APPROVED = 2
+STATUS_PROCESSING = 3
+STATUS_AWAITING_SUPPLEMENT = 4
+STATUS_AWAITING_VALIDATION = 5
+STATUS_REJECTED = 6
+STATUS_RECEIVED = 7
+
+
+STATUS_CHOICES = (
+    (STATUS_DRAFT, "Brouillon"),
+    (STATUS_SUBMITTED_FOR_VALIDATION, "Envoyée, en attente de traitement"),
+    (STATUS_AWAITING_SUPPLEMENT, "Demande de compléments"),
+    (STATUS_PROCESSING, "En traitement"),
+    (STATUS_AWAITING_VALIDATION, "En validation"),
+    (STATUS_APPROVED, "Approuvée"),
+    (STATUS_REJECTED, "Refusée"),
+    (STATUS_RECEIVED, "Annonce réceptionnée"),
+)
 
 
 def insert_workflow_status(apps, schema_editor):
     """
     Insert values in PermitWorkflowStatus model from models.PermitRequest.STATUS_CHOICES values
     """
-    for status_value in permitmodels.PermitRequest.STATUS_CHOICES:
-        for entity in permitmodels.PermitAdministrativeEntity.objects.all():
-            permitmodels.PermitWorkflowStatus.objects.get_or_create(
-                    status=status_value[0],
-                    administrative_entity=entity)
+    for status_value in STATUS_CHOICES:
+
+        AdministrativeEntity = apps.get_model("permits", "PermitAdministrativeEntity")
+        PermitWorkflowStatus = apps.get_model("permits", "PermitWorkflowStatus")
+
+        for entity in AdministrativeEntity.objects.all():
+            PermitWorkflowStatus.objects.get_or_create(
+                status=status_value[0],
+                administrative_entity=entity)
 
 
 class Migration(migrations.Migration):
