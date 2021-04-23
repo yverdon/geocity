e984c4980f7c2003130452a39cfc7a5697b6449f
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-04-23 15:59:11 +0200

    rename, comment and add new field for layers that have to be printed in pdf - models

--- permits/models.py
+++ permits/models.py
@@ -786,7 +786,8 @@ class PermitRequestAmendPropertyValue(models.Model):
 
 
 class QgisTemplate(models.Model):
-    qgis_file = models.FileField(upload_to="qgis_templates")
-    qgis_name = models.CharField(max_length=150)
+    qgis_project_file = models.FileField(_("Fichier QGIS '*.qgs'"), upload_to="qgis_templates",)
+    qgis_print_template_name = models.CharField(_("Nom du template d'impression QGIS"), max_length=150,)
+    qgis_layers = models.CharField(_("Liste des couches QGIS à afficher séparées par les virgules ','"), max_length=250,)
     description = models.CharField(max_length=150)
     works_object_type = models.ForeignKey(WorksObjectType, on_delete=models.CASCADE)
