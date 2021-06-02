421cddf615104ea2bdb014fde2f25abcf948bdb8
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-06-02 12:20:30 +0200

    change help text*

--- permits/migrations/0022_add_field_tags_to_permitadministrativeentity_add_requires_payment_field_to_works_object_type_model.py
+++ permits/migrations/0022_add_field_tags_to_permitadministrativeentity_add_requires_payment_field_to_works_object_type_model.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='permitadministrativeentity',
             name='tags',
-            field=taggit.managers.TaggableManager(blank=True, help_text='A comma-separated list of tags.', through='taggit.TaggedItem', to='taggit.Tag', verbose_name='Mots-clés'),
+            field=taggit.managers.TaggableManager(blank=True, help_text="Mots clefs sans espaces, séparés par des virgules permettant ensuite de filtrer les entités par l'url: https://geocity.ch&filter=yverdon&filter=permis-de-fouille", through='taggit.TaggedItem', to='taggit.Tag', verbose_name='Mots-clés'),
         ),
         migrations.AddField(
             model_name='worksobjecttype',
