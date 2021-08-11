874fc195a12dda091bf68e77dba255feca67909c
T4m <matthieu.cornut@liip.ch>
2021-08-11 11:55:40 +0200

    YC-350 Mapnv oauth2 fix completed redirect

--- mapnv/views.py
+++ mapnv/views.py
@@ -1,4 +1,5 @@
 import requests
+from allauth.socialaccount.models import SocialLogin
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2Adapter,
     OAuth2LoginView,
@@ -18,12 +19,11 @@ class MapnvAdapter(OAuth2Adapter):
     authorize_url = "{}/oauth/login".format(settings.AUTH_PROVIDER_MAPNV_URL)
     profile_url = "{}/loginuser".format(settings.AUTH_PROVIDER_MAPNV_URL)
 
-    def complete_login(self, request, app, token, **kwargs):
+    def complete_login(self, request, app, token, **kwargs) -> SocialLogin:
         headers = {"Authorization": "Bearer {0}".format(token.token)}
         resp = requests.get(self.profile_url, headers=headers)
         extra_data = resp.json()
-        new_user = self.get_provider().sociallogin_from_response(request, extra_data)
-        return new_user
+        return self.get_provider().sociallogin_from_response(request, extra_data)
 
 
 oauth2_login = OAuth2LoginView.adapter_view(MapnvAdapter)
