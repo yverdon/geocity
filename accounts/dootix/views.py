a046bbc235cbf53e9cab253be0816319500b94e2
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-09-02 15:48:46 +0200

    remove redirect_uri

--- accounts/dootix/views.py
+++ accounts/dootix/views.py
@@ -2,21 +2,39 @@ import requests
 from allauth.socialaccount.models import SocialLogin
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2Adapter,
+    OAuth2Client,
     OAuth2LoginView,
     OAuth2CallbackView,
 )
 from .provider import DootixProvider
 from django.conf import settings
+from django.utils.http import urlencode
+
+
+# Dootix oAuth backend does not accept redirect_uri param in request, thus, remove it
+class OAuth2ClientCustom(OAuth2Client):
+    def get_redirect_url(self, authorization_url, extra_params):
+        params = {
+            "client_id": self.consumer_key,
+            "scope": self.scope,
+            "response_type": "code",
+        }
+        if self.state:
+            params["state"] = self.state
+        params.update(extra_params)
+        return "%s?%s" % (authorization_url, urlencode(params))
 
 
 class DootixAdapter(OAuth2Adapter):
     provider_id = DootixProvider.id
 
+    client_class = OAuth2ClientCustom
+
     # Fetched programmatically, must be reachable from container
     access_token_url = "{}/oauth/token".format(settings.AUTH_PROVIDER_DOOTIX_URL)
 
     # URL to reach Dootix login form
-    authorize_url = "{}/login".format(settings.AUTH_PROVIDER_DOOTIX_URL)
+    authorize_url = "{}/oauth/authorize".format(settings.AUTH_PROVIDER_DOOTIX_URL)
     profile_url = "{}/api/user".format(settings.AUTH_PROVIDER_DOOTIX_URL)
 
     def complete_login(self, request, app, token, **kwargs) -> SocialLogin:
