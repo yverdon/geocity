4a8428d337cd2495ccc1b80d417d3271e71cf470
Felice Candilio <felicemcc@gmail.com>
2021-10-15 20:04:51 +0200

    SPRINT-8/MAIN Fix migrations order

--- permits/migrations/0043_worksobjecttype_start_delay.py
+++ permits/migrations/0043_worksobjecttype_start_delay.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('permits', '0041_permit_request_indexes'),
+        ('permits', '0042_add_regex_to_worksobjectproperty'),
     ]
 
     operations = [
