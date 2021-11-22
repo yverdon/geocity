847878b5f3743948b58ec4841d8ee7d58f384a33
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-11-22 13:54:58 +0100

    refactor expressions

--- qgisserver/plugins/geocity_expressions/__init__.py
+++ qgisserver/plugins/geocity_expressions/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 def serverClassFactory(serverIface):
     from .geocity_expressions import GeocityExpressions
+
     return GeocityExpressions(serverIface)
--- qgisserver/plugins/geocity_expressions/geocity_expressions.py
+++ qgisserver/plugins/geocity_expressions/geocity_expressions.py
@@ -1,40 +1,166 @@
 # -*- coding: utf-8 -*-
-
-from qgis.core import QgsMessageLog, Qgis, QgsExpression
+from qgis.core import *
 from qgis.utils import qgsfunction
 from datetime import datetime
-
-@qgsfunction(args='auto', group='Geocity')
-def permit_properties(feature, parent):
-    """
-    Format the json values in permit_properties field
-    <h2>Example usage:</h2>
-    <ul>
-      permit_properties()
-    </ul>
-    """
-    # TODO: return standard / basic layout
-    return 'Hello permit_properties!!!'
+import json
 
 
-@qgsfunction(args='auto', group='Geocity')
-def amend_properties(feature, parent):
+@qgsfunction(args="auto", group="Geocity")
+def get_permit_amend_properties(feature, parent):
     """
-    Format the json values in amend_properties field
-    <h2>Example usage:</h2>
-    <ul>
-      amend_properties()
-    </ul>
+    Function to get a string output from a list of actors
     """
-    # TODO: return standard / basic layout
-    return 'Hello amend_properties!!!'
+    get_keys = True
+    field_names = [field.name() for field in feature.fields()]
+    d = dict(zip(field_names, feature.attributes()))
+    amend_properties = json.loads(d["amend_properties"])
+    retval = ""
+    lr = "<br>"
+    for i, amend_property in amend_properties.items():
+        strline = f"<h3>{i}</h3>"
+        retval += strline
+        for k, v in amend_property.items():
+            if get_keys:
+                strline = (
+                    f"<dl><dt>{lr}property_{k}:</dt> <dd>{v}{lr}</dd></dl>"
+                    if str(k) == "id"
+                    else f"<dl><dt>{k}:</dt> <dd>{v}{lr}</dd></dl>"
+                )
+            else:
+                strline = (
+                    f"<dl><dt>property_{v}{lr}</dt></dl>"
+                    if str(k) == "id"
+                    else f"<dl><dt>{v}{lr}</dt></dl>"
+                )
+            retval += strline
 
-    resfresh_datetime = datetime.now()
-    return str(resfresh_datetime)
+    return retval
+
+
+@qgsfunction(args="auto", group="Geocity")
+def get_permit_author(feature, parent):
+    """
+    Function to get a string output from a list of actors
+    """
+    get_keys = False
+    field_names = [field.name() for field in feature.fields()]
+    d = dict(zip(field_names, feature.attributes()))
+    author = json.loads(d["author"])
+    retval = ""
+    lr = "<br>"
+    for i, author in author.items():
+        if get_keys:
+            strline = (
+                f"{lr}author_{i}: {author}{lr}"
+                if str(i) == "id"
+                else f"{i}: {author}{lr}"
+            )
+        else:
+            strline = f"{author}{lr}" if str(i) == "id" else f"{author}{lr}"
+        retval += strline
+
+    return retval
+
+
+@qgsfunction(args="auto", group="Geocity")
+def get_permit_contacts(pos, feature, parent):
+    """
+    Function to get a string output from a list of actors
+    """
+    try:
+        pos
+    except NameError:
+        pos = 0
+
+    get_keys = False
+    field_names = [field.name() for field in feature.fields()]
+    d = dict(zip(field_names, feature.attributes()))
+    permit_request_actors = json.loads(d["permit_request_actor"])
+    retval = ""
+    lr = "<br>"
+    for i, (j, actor) in enumerate(permit_request_actors.items()):
+        if pos == i:
+            strline = f"<h3>{j}</h3>"
+            retval += strline
+            for k, v in actor.items():
+                if get_keys:
+                    strline = (
+                        f"{lr}contact_{k}: {v}{lr}"
+                        if str(k) == "id"
+                        else f"{k}: {v}{lr}"
+                    )
+                else:
+                    strline = f"{v}{lr}" if str(k) == "id" else f"{v}{lr}"
+                retval += strline
+
+    return retval
+
+
+@qgsfunction(args="auto", group="Geocity")
+def get_permit_request_properties(feature, parent):
+    """
+    Function to get a string output from a list of actors
+    """
+    get_keys = True
+    field_names = [field.name() for field in feature.fields()]
+    d = dict(zip(field_names, feature.attributes()))
+    request_properties = json.loads(d["request_properties"])
+    retval = ""
+    lr = "<br>"
+    for i, request_property in request_properties.items():
+        strline = f"<h3>{i}</h3>"
+        retval += strline
+        for k, v in request_property.items():
+            if get_keys:
+                strline = (
+                    f"{lr}property_{k}: {v}{lr}" if str(k) == "id" else f"{k}: {v}{lr}"
+                )
+            else:
+                strline = f"{v}{lr}" if str(k) == "id" else f"{v}{lr}"
+            retval += strline
+
+    return retval
+
+
+@qgsfunction(args="auto", group="Geocity")
+def get_permit_validations(feature, parent):
+    """
+    Function to get a string output from a list of actors
+    """
+    get_keys = True
+    field_names = [field.name() for field in feature.fields()]
+    d = dict(zip(field_names, feature.attributes()))
+    validations = json.loads(d["validations"])
+    retval = ""
+    lr = "<br>"
+    for i, (j, validation) in enumerate(validations.items()):
+        strline = f"<h3>{j}</h3>"
+        retval += strline
+        for k, v in validation.items():
+            if get_keys:
+                strline = (
+                    f"<dl><dt>{lr}validation_{k}:</dt> <dd>{v}{lr}</dd></dl>"
+                    if str(k) == "id"
+                    else f"<dl><dt>{k}:</dt> <dd>{v}{lr}</dd></dl>"
+                )
+            else:
+                strline = (
+                    f"<dl><dt>validation_{v}{lr}</dt></dl>"
+                    if str(k) == "id"
+                    else f"<dl><dt>{v}{lr}</dt></dl>"
+                )
+            retval += strline
+
+    return retval
 
 
 class GeocityExpressions:
     def __init__(self, serverIface):
-        QgsMessageLog.logMessage('******Loading expressions********', 'GeocityExpressions', Qgis.Info)
-        QgsExpression.registerFunction(permit_properties)
-        QgsExpression.registerFunction(amend_properties)
+        QgsMessageLog.logMessage(
+            "******Loading expressions********", "GeocityExpressions", Qgis.Info
+        )
+        QgsExpression.registerFunction(get_permit_amend_properties)
+        QgsExpression.registerFunction(get_permit_author)
+        QgsExpression.registerFunction(get_permit_contacts)
+        QgsExpression.registerFunction(get_permit_request_properties)
+        QgsExpression.registerFunction(get_permit_validations)
