251c43751243563b1b4d9a0e426b3c8cd24a21fa
nicolas-sitylb <nbl@ylb.ch>
2021-11-24 17:01:50 +0100

    Update qgis atlas expressions.

--- qgisserver/plugins/geocity_expressions/geocity_expressions.py
+++ qgisserver/plugins/geocity_expressions/geocity_expressions.py
@@ -15,6 +15,7 @@ def get_permit_amend_properties(feature, parent):
     d = dict(zip(field_names, feature.attributes()))
     amend_properties = json.loads(d['amend_properties'])
     retval = ''
+    retval += "<style>body{font-family: arial; font-size: 12px;}</style>"
     print(f"amend_properties: {amend_properties}")
     lr = '<br>'
     for i, amend_property in amend_properties.items():
@@ -40,9 +41,11 @@ def get_permit_author(feature, parent):
     d = dict(zip(field_names, feature.attributes()))
     author = json.loads(d['author'])
     retval = ''
+    retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
     lr = '<br>'
     ts = '&#8239;'
     for i, (k, author) in enumerate(author.items()):
+        retval += "<table>"
         if get_keys:
             strline = f"{lr}author_{k}: {author}{lr}" if str(k) == 'id' else f"{k}: {author}{lr}"
         else:
@@ -58,8 +61,10 @@ def get_permit_author(feature, parent):
                 f"Téléphone (2){ts}:",
                 f"E-mail{ts}:"
             ]
-            strline = f"{keys[i]} {author}{lr}"
+            #strline = f"{author}{lr}" if str(i) == 'id' else f"{author}{lr}"
+            strline = f"<tr><th>{keys[i]}</th> <td>{author}</td></tr>"
         retval += strline
+        retval += "</table>"
 
     return(retval)
 
@@ -79,6 +84,7 @@ def get_permit_contacts(pos, feature, parent):
     d = dict(zip(field_names, feature.attributes()))
     permit_request_actors = json.loads(d['permit_request_actor'])
     retval = ''
+    retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
     print(f"permit_request_actors: {permit_request_actors}")
     lr = '<br>'
     ts = '&#8239;'
@@ -86,6 +92,7 @@ def get_permit_contacts(pos, feature, parent):
         if (pos==i):
             strline = f"<h3>{j}</h3>"
             retval += strline
+            retval += "<table>"
             for idx, (k, v) in enumerate(actor.items()):
                 if get_keys:
                     strline = f"{lr}contact_{k}: {v}{lr}" if str(k) == 'id' else f"{k}: {v}{lr}"
@@ -103,8 +110,9 @@ def get_permit_contacts(pos, feature, parent):
                         f"Téléphone{ts}:",
                         f"E-mail{ts}:"
                     ]
-                    strline = f"{keys[idx]} contact_{v}{lr}" if str(k) == 'id' else f"{keys[idx]} {v}{lr}"
+                    strline = f"<tr><th>{keys[idx]}</th> <td>contact_{v}</td></tr>" if str(k) == 'id' else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
                 retval += strline
+            retval += "</table>"
 
     return(retval)
 
@@ -119,17 +127,33 @@ def get_permit_request_properties(feature, parent):
     d = dict(zip(field_names, feature.attributes()))
     request_properties = json.loads(d['request_properties'])
     retval = ''
+    retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
     print(f"request_properties: {request_properties}")
     lr = '<br>'
+    ts = '&#8239;'
     for i, request_property in request_properties.items():
         strline = f"<h3>{i}</h3>"
         retval += strline
-        for k, v in request_property.items():
+        retval += "<table>"
+        for idx, (k, v) in enumerate(request_property.items()):
             if get_keys:
-                strline = f"{lr}property_{k}: {v}{lr}" if str(k) == 'id' else f"{k}: {v}{lr}"
+                strline = f"<tr><th>{k}</th> <td>property_{v}</td></th>" if str(k) == 'id' else f"<tr><th>{k}</th> <td>{v}</td></tr>"
             else:
-                strline = f"{v}{lr}" if str(k) == 'id' else f"{v}{lr}"
+                keys = [
+                        f"Largeur [m]{ts}:",
+                        f"Longueur [m]{ts}:",
+                        f"Marquage routier endommmagé{ts}:",
+                        f"Moins de 3m d'un tronc d'arbre ou haie{ts}:",
+                        f"Sur la chaussée{ts}:",
+                        f"Sur une surface verte{ts}:",
+                        f"Sur un trottoir{ts}:",
+                        f"Description{ts}:",
+                        f"Documents complémentaires{ts}:"
+                    ]
+                strline = f"<tr><th>{v}</th></tr>" if str(k) == 'id' else f"<tr><th>{v}</th></tr>"
+                strline = f"<tr><th>{keys[idx]}</th> <td>property_{v}</td></th>" if str(k) == 'id' else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
             retval += strline
+        retval += "</table>"
 
     return(retval)
 
@@ -144,6 +168,7 @@ def get_permit_validations(feature, parent):
     d = dict(zip(field_names, feature.attributes()))
     validations = json.loads(d['validations'])
     retval = ''
+    retval += "<style>body{font-family: arial; font-size: 12px;}</style>"
     lr = '<br>'
     ts = '&#8239;'
     for i, (j, validation) in enumerate(validations.items()):
