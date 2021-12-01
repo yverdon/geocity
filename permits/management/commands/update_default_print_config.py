9e741752e0d0b96220198bcd71c09648f6909d1f
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-12-01 15:06:24 +0100

    add update print template function

--- permits/management/commands/update_default_print_config.py
+++ permits/management/commands/update_default_print_config.py
@@ -2,18 +2,6 @@ from permits import models
 from django.core import management
 from django.core.management.base import BaseCommand
 
-
-def add_default_print_config():
-
-    for wot in models.WorksObjectType.objects.all():
-        wot.qgisproject_set.create(
-            qgis_project_file="report_template.qgs",
-            qgis_print_template_name="print_template",
-            qgis_layers="background,permits,permits_point,permits_line,permits_poly",
-            description="Impression standard",
-        )
-        wot.save()
-
 def update_default_print_config():
 
     for wot in models.WorksObjectType.objects.all():
@@ -28,7 +16,6 @@ def update_default_print_config():
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(
-            "Adding default print configuration for work object types that have none"
+            "Updating default print configuration for work object types"
         )
-        add_default_print_config()
         update_default_print_config()
