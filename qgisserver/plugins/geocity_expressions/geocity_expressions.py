5bf652cc526f9c6aa0cf82589e50f59c6bf5986a
nicolas-sitylb <nbl@ylb.ch>
2021-12-03 15:00:45 +0100

    Update functions to return an empty string if no keys was found.

--- qgisserver/plugins/geocity_expressions/geocity_expressions.py
+++ qgisserver/plugins/geocity_expressions/geocity_expressions.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import json
 
 
-@qgsfunction(args="auto", group="Geocity")
+@qgsfunction(args='auto', group='Geocity')
 def get_permit_amend_properties(feature, parent):
     """
     Function to get an HTML string output from the properties.amend_properties
@@ -23,36 +23,31 @@ def get_permit_amend_properties(feature, parent):
     get_keys = True
     field_names = [field.name() for field in feature.fields()]
     d = dict(zip(field_names, feature.attributes()))
-    amend_properties = json.loads(d["amend_properties"])
-    retval = ""
+    amend_properties = json.loads(d['amend_properties'])
+    retval = ''
     retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
-    # print(f"amend_properties: {amend_properties}")
-    lr = "<br>"
-    ts = "&#8239;"
+    #print(f"amend_properties: {amend_properties}")
+    lr = '<br>'
+    ts = '&#8239;'
+    has_data = False
     for i, amend_property in amend_properties.items():
         strline = f"<h3>{i}</h3>"
         retval += strline
         retval += "<table>"
         for k, v in amend_property.items():
+            has_data = True if k is not None else False
             if get_keys:
-                strline = (
-                    f"<tr><th>property_{k}</th> <td>{v}</td></th>"
-                    if str(k) == "id"
-                    else f"<tr><th>{k}</th> <td>{v}</td></tr>"
-                )
+                strline = f"<tr><th>property_{k}</th> <td>{v}</td></th>" if str(k) == 'id' else f"<tr><th>{k}</th> <td>{v}</td></tr>"
             else:
-                strline = (
-                    f"<tr><th>property_{keys[idx]}</th> <td>{v}</td></th>"
-                    if str(k) == "id"
-                    else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
-                )
+                strline = f"<tr><th>property_{keys[idx]}</th> <td>{v}</td></th>" if str(k) == 'id' else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
             retval += strline
         retval += "</table>"
+        if not has_data: retval = ''
 
     return retval
 
 
-@qgsfunction(args="auto", group="Geocity")
+@qgsfunction(args='auto', group='Geocity')
 def get_permit_author(feature, parent):
     """
     Function to get an HTML string output from the properties.author
@@ -70,19 +65,17 @@ def get_permit_author(feature, parent):
     get_keys = False
     field_names = [field.name() for field in feature.fields()]
     d = dict(zip(field_names, feature.attributes()))
-    author = json.loads(d["author"])
-    retval = ""
+    author = json.loads(d['author'])
+    retval = ''
     retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
-    lr = "<br>"
-    ts = "&#8239;"
+    lr = '<br>'
+    ts = '&#8239;'
+    has_data = False
     for idx, (k, v) in enumerate(author.items()):
+        has_data = True if k is not None else False
         retval += "<table>"
         if get_keys:
-            strline = (
-                f"<tr><th>author_{k}:</th> <td>{v}</td></tr>"
-                if str(k) == "id"
-                else f"<tr><th>{k}:</th> <td>{v}</td></tr>"
-            )
+            strline = f"<tr><th>author_{k}:</th> <td>{v}</td></tr>" if str(k) == 'id' else f"<tr><th>{k}:</th> <td>{v}</td></tr>"
         else:
             keys = [
                 f"Prénom{ts}:",
@@ -94,17 +87,18 @@ def get_permit_author(feature, parent):
                 f"Numéro TVA{ts}:",
                 f"Téléphone{ts}:",
                 f"Téléphone (2){ts}:",
-                f"E-mail{ts}:",
+                f"E-mail{ts}:"
             ]
-            # strline = f"{v}{lr}" if str(idx) == 'id' else f"{v}{lr}"
-            strline = f"<tr><th>{keys[idx]}</th><td>{v}</td></tr>"
+            #strline = f"{v}{lr}" if str(idx) == 'id' else f"{v}{lr}"
+            strline = f'<tr><th>{keys[idx]}</th><td>{v}</td></tr>'
         retval += strline
         retval += "</table>"
+        if not has_data: retval = ''
 
     return retval
 
 
-@qgsfunction(args="auto", group="Geocity")
+@qgsfunction(args='auto', group='Geocity')
 def get_permit_contacts(pos, feature, parent):
     """
     Function to get an HTML string output from the properties.permit_request_actor
@@ -129,24 +123,22 @@ def get_permit_contacts(pos, feature, parent):
     get_keys = False
     field_names = [field.name() for field in feature.fields()]
     d = dict(zip(field_names, feature.attributes()))
-    permit_request_actors = json.loads(d["permit_request_actor"])
-    retval = ""
+    permit_request_actors = json.loads(d['permit_request_actor'])
+    retval = ''
     retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
-    # print(f"permit_request_actors: {permit_request_actors}")
-    lr = "<br>"
-    ts = "&#8239;"
+    #print(f"permit_request_actors: {permit_request_actors}")
+    lr = '<br>'
+    ts = '&#8239;'
+    has_data = False
     for i, (j, actor) in enumerate(permit_request_actors.items()):
-        if pos == i:
+        if (pos==i):
             strline = f"<h3>{j}</h3>"
             retval += strline
             retval += "<table>"
             for idx, (k, v) in enumerate(actor.items()):
+                has_data = True if k is not None else False
                 if get_keys:
-                    strline = (
-                        f"<tr><th>contact_{k}</th> <td>{v}</td></th>"
-                        if str(k) == "id"
-                        else f"<tr><th>{k}</th> <td>{v}</td></tr>"
-                    )
+                    strline = f"<tr><th>contact_{k}</th> <td>{v}</td></th>" if str(k) == 'id' else f"<tr><th>{k}</th> <td>{v}</td></tr>"
                 else:
                     keys = [
                         f"Type{ts}:",
@@ -159,20 +151,17 @@ def get_permit_contacts(pos, feature, parent):
                         f"NPA{ts}:",
                         f"Localité{ts}:",
                         f"Téléphone{ts}:",
-                        f"E-mail{ts}:",
+                        f"E-mail{ts}:"
                     ]
-                    strline = (
-                        f"<tr><th>contact_{keys[idx]}</th> <td>{v}</td></tr>"
-                        if str(k) == "id"
-                        else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
-                    )
+                    strline = f"<tr><th>contact_{keys[idx]}</th> <td>{v}</td></tr>" if str(k) == 'id' else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
                 retval += strline
             retval += "</table>"
+            if not has_data: retval = ''
 
     return retval
 
 
-@qgsfunction(args="auto", group="Geocity")
+@qgsfunction(args='auto', group='Geocity')
 def get_permit_geotime(feature, parent):
     """
     Function to get an HTML string output from the properties.geotime_aggregated
@@ -190,30 +179,29 @@ def get_permit_geotime(feature, parent):
     get_keys = False
     field_names = [field.name() for field in feature.fields()]
     d = dict(zip(field_names, feature.attributes()))
-    geotime = json.loads(d["geotime_aggregated"])
-    retval = ""
+    geotime = json.loads(d['geotime_aggregated'])
+    retval = ''
     retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
-    lr = "<br>"
-    ts = "&#8239;"
+    lr = '<br>'
+    ts = '&#8239;'
+    has_data = False
     for idx, (k, v) in enumerate(geotime.items()):
+        has_data = True if k is not None else False
         retval += "<table>"
         if get_keys:
-            strline = (
-                f"<tr><th>geotime_{k}:</th> <td>{v}</td></tr>"
-                if str(k) == "id"
-                else f"<tr><th>{k}:</th> <td>{v}</td></tr>"
-            )
+            strline = f"<tr><th>geotime_{k}:</th> <td>{v}</td></tr>" if str(k) == 'id' else f"<tr><th>{k}:</th> <td>{v}</td></tr>"
         else:
             keys = [
                 f"Date de début{ts}:",
                 f"Date de fin{ts}:",
                 f"Commentaire{ts}:",
-                f"Lien externe{ts}:",
+                f"Lien externe{ts}:"
             ]
-            # strline = f"{v}{lr}" if str(idx) == 'id' else f"{v}{lr}"
-            strline = f"<tr><th>{keys[idx]}</th><td>{v}</td></tr>"
+            #strline = f"{v}{lr}" if str(idx) == 'id' else f"{v}{lr}"
+            strline = f'<tr><th>{keys[idx]}</th><td>{v}</td></tr>'
         retval += strline
         retval += "</table>"
+        if not has_data: retval = ''
 
     return retval
 
@@ -233,14 +221,13 @@ def translate_boolean(v):
         The corresponding translated string.
     """
     if isinstance(v, bool) and v:
-        v = "Oui"
+       v = "Oui"
     if isinstance(v, bool) and not v:
-        v = "Non"
+       v = "Non"
 
     return v
-
-
-@qgsfunction(args="auto", group="Geocity")
+               
+@qgsfunction(args='auto', group='Geocity')
 def get_permit_request_properties(feature, parent):
     """
     Function to get an HTML string output from the properties.request_properties
@@ -258,45 +245,44 @@ def get_permit_request_properties(feature, parent):
     get_keys = True
     field_names = [field.name() for field in feature.fields()]
     d = dict(zip(field_names, feature.attributes()))
-    request_properties = json.loads(d["request_properties"])
-    retval = ""
+    request_properties = json.loads(d['request_properties'])
+    retval = ''
     retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
-    # print(f"request_properties: {request_properties}")
-    lr = "<br>"
-    ts = "&#8239;"
+    #print(f"request_properties: {request_properties}")
+    lr = '<br>'
+    ts = '&#8239;'
+    has_data = False
     for i, request_property in request_properties.items():
         strline = f"<h3>{i}</h3>"
         retval += strline
         retval += "<table>"
         for idx, (k, v) in enumerate(request_property.items()):
+            has_data = True if k is not None else False
             v = translate_boolean(v)
             if get_keys:
-                strline = (
-                    f"<tr><th>property_{k}{ts}:</th> <td>{v}</td></th>"
-                    if str(k) == "id"
-                    else f"<tr><th>{k}{ts}:</th> <td>{v}</td></tr>"
-                )
+                strline = f"<tr><th>property_{k}{ts}:</th> <td>{v}</td></th>" if str(k) == 'id' else f"<tr><th>{k}{ts}:</th> <td>{v}</td></tr>"
             else:
                 keys = [
-                    f"Largeur [m]{ts}:",
-                    f"Longueur [m]{ts}:",
-                    f"Marquage routier endommmagé{ts}:",
-                    f"Moins de 3m d'un tronc d'arbre ou haie{ts}:",
-                    f"Sur la chaussée{ts}:",
-                    f"Sur une surface verte{ts}:",
-                    f"Sur un trottoir{ts}:",
-                    f"Description{ts}:",
-                    f"Documents complémentaires{ts}:",
-                ]
-                # strline = f"<tr><th>{v}</th></tr>" if str(k) == 'id' else f"<tr><th>{v}</th></tr>"
+                        f"Largeur [m]{ts}:",
+                        f"Longueur [m]{ts}:",
+                        f"Marquage routier endommmagé{ts}:",
+                        f"Moins de 3m d'un tronc d'arbre ou haie{ts}:",
+                        f"Sur la chaussée{ts}:",
+                        f"Sur une surface verte{ts}:",
+                        f"Sur un trottoir{ts}:",
+                        f"Description{ts}:",
+                        f"Documents complémentaires{ts}:"
+                    ]
+                #strline = f"<tr><th>{v}</th></tr>" if str(k) == 'id' else f"<tr><th>{v}</th></tr>"
                 strline = f"""<tr><th>property_{keys[idx]}</th> <td>{v}</td></th>" if str(k) == 'id' else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"""
             retval += strline
         retval += "</table>"
+        if not has_data: retval = ''
 
     return retval
 
 
-@qgsfunction(args="auto", group="Geocity")
+@qgsfunction(args='auto', group='Geocity')
 def get_permit_validations(feature, parent):
     """
     Function to get an HTML string output from the properties.validations
@@ -314,36 +300,31 @@ def get_permit_validations(feature, parent):
     get_keys = False
     field_names = [field.name() for field in feature.fields()]
     d = dict(zip(field_names, feature.attributes()))
-    validations = json.loads(d["validations"])
-    retval = ""
+    validations = json.loads(d['validations'])
+    retval = ''
     retval += "<style>body{font-family: arial; font-size: 12px;}th, td{padding: 0px;  text-align: left; font-size: 12px;}</style>"
-    lr = "<br>"
-    ts = "&#8239;"
+    lr = '<br>'
+    ts = '&#8239;'
+    has_data = False
     for i, (j, validation) in enumerate(validations.items()):
         strline = f"<h3>{j}</h3>"
         retval += strline
         retval += "<table>"
         for idx, (k, v) in enumerate(validation.items()):
+            has_data = True if k is not None else False
             if get_keys:
-                strline = (
-                    f"<tr><th>validation_{k}</th> <td>{v}</td></th>"
-                    if str(k) == "id"
-                    else f"<tr><th>{k}</th> <td>{v}</td></tr>"
-                )
+                strline = f"<tr><th>validation_{k}</th> <td>{v}</td></th>" if str(k) == 'id' else f"<tr><th>{k}</th> <td>{v}</td></tr>"
             else:
                 keys = [
                     f"Statut{ts}:",
                     f"Commentaire (avant){ts}:",
                     f"Commentaire (pendant){ts}:",
-                    f"Commentaire (après){ts}:",
+                    f"Commentaire (après){ts}:"
                 ]
-                strline = (
-                    f"<tr><th>validation_{keys[idx]}</th> <td>{v}</td></tr>"
-                    if str(k) == "id"
-                    else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
-                )
+                strline = f"<tr><th>validation_{keys[idx]}</th> <td>{v}</td></tr>" if str(k) == 'id' else f"<tr><th>{keys[idx]}</th> <td>{v}</td></tr>"
             retval += strline
         retval += "</table>"
+        if not has_data: retval = ''
 
     return retval
 
