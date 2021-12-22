6a64082302f1cb3cb8ab7a6011c24b73f56383f5
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2021-12-22 12:50:21 +0100

    hide validator details to user

--- permits/views.py
+++ permits/views.py
@@ -217,16 +217,15 @@ class PermitRequestDetailView(View):
             kwargs["validations"] = self.permit_request.validations.select_related(
                 "department", "department__group"
             )
-
-
+        can_validate_permit_request = services.can_validate_permit_request(
+            self.request.user, self.permit_request
+        )
         history = (
             self.permit_request.history.all()
             if services.has_permission_to_amend_permit_request(
                 self.request.user, self.permit_request
             )
-            or services.can_validate_permit_request(
-                self.request.user, self.permit_request
-            )
+            or can_validate_permit_request
             else None
         )
 
@@ -243,6 +242,7 @@ class PermitRequestDetailView(View):
                 "can_classify": services.can_classify_permit_request(
                     self.request.user, self.permit_request
                 ),
+                "can_validate_permit_request": can_validate_permit_request,
                 "print_templates": services.get_permit_request_print_templates(
                     self.permit_request
                 ),
