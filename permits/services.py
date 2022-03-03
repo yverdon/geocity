80c388b2d8de17fb1bf038f8a3412522d1a81680
Olivier Monod <olivier.monod@yverdon-les-bains.ch>
2022-03-03 14:44:30 +0100

    check original client ip when behind a proxy

--- permits/services.py
+++ permits/services.py
@@ -1503,19 +1503,29 @@ def clear_session_filters(request):
     request.session["typefilter"] = []
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
+
+
 def check_request_ip_is_allowed(request):
     """
     Check that the request is coming from allowed ip
     """
     # Check for exact ip
+    client_ip = get_client_ip(request)
     if config.IP_WHITELIST != "":
         for whitelisted_ip in config.IP_WHITELIST.split(","):
-            if request.META["REMOTE_ADDR"] in whitelisted_ip:
+            if client_ip in whitelisted_ip:
                 return True
     # Check for network
     if config.NETWORK_WHITELIST != "":
         for whitelisted_network in config.NETWORK_WHITELIST.split(","):
-            ip_address = ipaddress.ip_address(request.META["REMOTE_ADDR"])
+            ip_address = ipaddress.ip_address(client_ip)
             ip_network = ipaddress.ip_network(whitelisted_network)
             if ip_address in ip_network:
                 return True
