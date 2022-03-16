40f538ad91631b8726863ec542b9b48e7877a9eb
Alexandre <36194606+AlexandreJunod@users.noreply.github.com>
2022-03-16 15:33:48 +0100

    Update test_permit_request_api.py

--- permits/tests/test_permit_request_api.py
+++ permits/tests/test_permit_request_api.py
@@ -477,7 +477,7 @@ class PermitRequestAPITestCase(TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_current_user_returns_users_informations(self):
+    def test_current_user_returns_user_informations(self):
         self.client.login(username=self.admin_user.username, password="password")
         response = self.client.get(reverse("current_user-list"), {})
         response_json = response.json()
