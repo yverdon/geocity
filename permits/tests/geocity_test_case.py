eea45f9292440ca354aadd04059c7c5dae740de6
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-03-29 14:22:24 +0200

    YC-536 Clean up tests + add test for multiple doc upload

--- permits/tests/geocity_test_case.py
+++ permits/tests/geocity_test_case.py
@@ -8,23 +8,32 @@ class GeocityTestCase(TestCase):
     INTEGRATOR = 1
     SUPER_USER = 2
 
+    def setUp(self):
+        self.groups = {
+            self.SECRETARIAT: factories.SecretariatGroupFactory(),
+            self.INTEGRATOR: factories.IntegratorGroupFactory(),
+        }
+
     def get_user(self, email, password, group):
         if group == self.SECRETARIAT:
             return factories.SecretariatUserFactory(
                 email=email,
                 password=password,
-                groups=[factories.SecretariatGroupFactory()],
+                groups=[self.groups[group]],
             )
         elif group == self.INTEGRATOR:
             return factories.IntegratorUserFactory(
                 email=email,
                 password=password,
-                groups=[factories.IntegratorGroupFactory()],
+                groups=[self.groups[group]],
             )
         elif group == self.SUPER_USER:
             return factories.SuperUserFactory(email=email, password=password)
         else:
-            return factories.UserFactory(email=email, password=password,)
+            return factories.UserFactory(
+                email=email,
+                password=password,
+            )
 
     def login(self, email, password="password", group=None):
         self.user = self.get_user(email=email, password=password, group=group)
