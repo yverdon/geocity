58e7a46db565c3c48f2532ff1cfeae6fb4e6c129
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-03-29 14:22:24 +0200

    YC-536 Clean up tests + add test for multiple doc upload

--- permits/tests/functional/test_complementary_documents.py
+++ permits/tests/functional/test_complementary_documents.py
@@ -5,65 +5,84 @@ from django.urls import reverse
 
 
 class TestComplementaryDocuments(GeocityTestCase):
+
+    management_form = {
+        "form-TOTAL_FORMS": ["1"],
+        "form-INITIAL_FORMS": ["0"],
+        "form-MIN_NUM_FORMS": ["0"],
+        "form-MAX_NUM_FORMS": ["1000"],
+        "action": "complementary_documents",
+        "save_continue": "",
+    }
+
     def setUp(self):
-        self.works_types = factories.WorksTypeFactory.create_batch(2)
-        self.works_objects = factories.WorksObjectFactory.create_batch(2)
+        super().setUp()
 
-        models.WorksObjectType.objects.create(
-            works_type=self.works_types[0],
-            works_object=self.works_objects[0],
-            is_public=True,
-        )
-        models.WorksObjectType.objects.create(
-            works_type=self.works_types[1],
-            works_object=self.works_objects[1],
-            is_public=True,
-        )
-
-    def test_pilot_can_upload_single_complementary_document(self):
-        self.login(email="user@test.com", group=self.SECRETARIAT)
-        secretary_group = factories.GroupFactory(name="Secrétariat")
-        department = factories.PermitDepartmentFactory(
-            group=secretary_group, is_backoffice=True
-        )
-        permit_request = factories.PermitRequestFactory(
+        self.login(email="user@test.com")
+        self.department = models.PermitDepartment.objects.filter(
+            group=self.groups[self.SECRETARIAT]
+        ).get()
+        self.permit_request = factories.PermitRequestFactory(
             author=self.user.permitauthor,
             status=models.PermitRequest.STATUS_RECEIVED,
-            administrative_entity=department.administrative_entity,
+            administrative_entity=self.department.administrative_entity,
         )
+        self.permit_request.refresh_from_db()
 
-        with open("permits/tests/files/real_pdf.pdf", "rb") as file:
-            response = self.client.post(
-                reverse(
-                    "permits:permit_request_detail",
-                    kwargs={"permit_request_id": permit_request.pk},
-                ),
-                data={
-                    "form-TOTAL_FORMS": ["1"],
-                    "form-INITIAL_FORMS": ["0"],
-                    "form-MIN_NUM_FORMS": ["0"],
-                    "form-MAX_NUM_FORMS": ["1000"],
-                    "form-0-description": ["Form description"],
-                    "form-0-status": [
-                        models.PermitRequestComplementaryDocument.STATUS_OTHER
-                    ],
-                    "form-0-authorised_departments": ["3"],
-                    "form-0-is_public": ["0"],
-                    "form-0-document": [file],
-                    "action": "complementary_documents",
-                    "save_continue": "",
-                },
-            )
-        permit_request.refresh_from_db()
+    def execute_complementary_document_upload_test(self, data):
+        self.login(email="pilot@test.com", group=self.SECRETARIAT)
+
+        response = self.client.post(
+            reverse(
+                "permits:permit_request_detail",
+                kwargs={"permit_request_id": self.permit_request.pk},
+            ),
+            data={**self.management_form, **data},
+        )
         self.assertEqual(response.status_code, 302)
 
         permit_request_detail = self.client.get(
             reverse(
                 "permits:permit_request_detail",
-                kwargs={"permit_request_id": permit_request.pk},
+                kwargs={"permit_request_id": self.permit_request.pk},
             )
         )
         expected = "<div class='alert alert-success'>Les documents ont bien été ajoutés à la demande #{pk}.</div>".format(
-            pk=permit_request.pk,
+            pk=self.permit_request.pk,
         )
         self.assertInHTML(expected, permit_request_detail.content.decode())
+
+    def test_pilot_can_upload_single_complementary_document(self):
+        with open("permits/tests/files/real_pdf.pdf", "rb") as file:
+            data = {
+                "form-0-description": ["Single document upload"],
+                "form-0-status": [
+                    models.PermitRequestComplementaryDocument.STATUS_OTHER
+                ],
+                "form-0-authorised_departments": [self.department.pk],
+                "form-0-is_public": ["0"],
+                "form-0-document": [file],
+            }
+
+            self.execute_complementary_document_upload_test(data)
+
+    def test_pilot_can_upload_multiple_complementary_documents(self):
+        with open("permits/tests/files/real_pdf.pdf", "rb") as file:
+            data = {
+                "form-0-description": ["Multiple document upload. #1"],
+                "form-0-status": [
+                    models.PermitRequestComplementaryDocument.STATUS_OTHER
+                ],
+                "form-0-authorised_departments": [self.department.pk],
+                "form-0-is_public": ["0"],
+                "form-0-document": [file],
+                "form-1-description": ["Multiple document upload. #2"],
+                "form-1-status": [
+                    models.PermitRequestComplementaryDocument.STATUS_FINALE
+                ],
+                "form-1-authorised_departments": [self.department.pk],
+                "form-1-is_public": ["0"],
+                "form-1-document": [file],
+            }
+
+            self.execute_complementary_document_upload_test(data)
