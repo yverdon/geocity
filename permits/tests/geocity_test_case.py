faa360ae9fb64ee3fff36f2ef73a8b000c9aaddf
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-03-31 10:56:11 +0200

    fix formatting

--- permits/tests/geocity_test_case.py
+++ permits/tests/geocity_test_case.py
@@ -17,23 +17,16 @@ class GeocityTestCase(TestCase):
     def get_user(self, email, password, group):
         if group == self.SECRETARIAT:
             return factories.SecretariatUserFactory(
-                email=email,
-                password=password,
-                groups=[self.groups[group]],
+                email=email, password=password, groups=[self.groups[group]],
             )
         elif group == self.INTEGRATOR:
             return factories.IntegratorUserFactory(
-                email=email,
-                password=password,
-                groups=[self.groups[group]],
+                email=email, password=password, groups=[self.groups[group]],
             )
         elif group == self.SUPER_USER:
             return factories.SuperUserFactory(email=email, password=password)
         else:
-            return factories.UserFactory(
-                email=email,
-                password=password,
-            )
+            return factories.UserFactory(email=email, password=password,)
 
     def login(self, email, password="password", group=None):
         self.user = self.get_user(email=email, password=password, group=group)
