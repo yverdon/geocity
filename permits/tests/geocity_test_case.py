d27457cdfc931989ba58144be3d6af348f4ecd92
Doran Kayoumi <doran.kayoumi@liip.ch>
2022-03-31 17:31:57 +0200

    YC-536 Improve setUp

--- permits/tests/geocity_test_case.py
+++ permits/tests/geocity_test_case.py
@@ -6,22 +6,46 @@ from permits.tests import factories
 class GeocityTestCase(TestCase):
     SECRETARIAT = 0
     INTEGRATOR = 1
-    SUPER_USER = 2
+    VALIDATOR = 2
+    SUPER_USER = 3
 
     def setUp(self):
+        self.administrative_entity = factories.PermitAdministrativeEntityFactory()
         self.groups = {
-            self.SECRETARIAT: factories.SecretariatGroupFactory(),
-            self.INTEGRATOR: factories.IntegratorGroupFactory(),
+            self.SECRETARIAT: factories.SecretariatGroupFactory.create(department=None),
+            self.INTEGRATOR: factories.IntegratorGroupFactory.create(department=None),
+            self.VALIDATOR: factories.ValidatorGroupFactory.create(department=None),
+        }
+        self.departments = {
+            self.SECRETARIAT: factories.PermitDepartmentFactory(
+                administrative_entity=self.administrative_entity,
+                is_backoffice=True,
+                group=self.groups[self.SECRETARIAT],
+            ),
+            self.INTEGRATOR: factories.PermitDepartmentFactory(
+                administrative_entity=self.administrative_entity,
+                is_integrator_admin=True,
+                group=self.groups[self.INTEGRATOR],
+            ),
+            self.VALIDATOR: factories.PermitDepartmentFactory(
+                administrative_entity=self.administrative_entity,
+                is_validator=True,
+                group=self.groups[self.VALIDATOR],
+            ),
         }
 
     def get_user(self, email, password, group):
         if group == self.SECRETARIAT:
-            return factories.SecretariatUserFactory(
+            return factories.SecretariatUserFactory.create(
                 email=email, password=password, groups=[self.groups[group]],
             )
         elif group == self.INTEGRATOR:
-            return factories.IntegratorUserFactory(
+            return factories.IntegratorUserFactory.create(
                 email=email, password=password, groups=[self.groups[group]],
+            )
+        elif group == self.VALIDATOR:
+            return factories.ValidatorUserFactory.create(
+                email=email, password=password, groups=[self.groups[group]]
             )
         elif group == self.SUPER_USER:
             return factories.SuperUserFactory(email=email, password=password)
