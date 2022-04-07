9c043204a3ea423e982599c9ad07c4c4dce3e391
Rémi Bovard <remi.bovard@vevey.ch>
2022-04-07 12:36:37 +0200

    Fix display of tags

--- permits/admin.py
+++ permits/admin.py
@@ -977,7 +977,7 @@ class PermitAdministrativeEntityAdmin(IntegratorFilterMixin, admin.ModelAdmin):
         "expeditor_name",
         "expeditor_email",
         "ofs_id",
-        "tags",
+        "get__tags",
     ]
 
     def sortable_str(self, obj):
@@ -987,6 +987,11 @@ class PermitAdministrativeEntityAdmin(IntegratorFilterMixin, admin.ModelAdmin):
     sortable_str.short_description = (
         "1.1 Configuration de l'entité administrative (commune, organisation)"
     )
+
+    def get__tags(self, obj):
+        return list(obj.tags.all())
+
+    get__tags.short_description = _("Mots-clés")
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "integrator":
@@ -1102,7 +1107,7 @@ class WorksTypeAdmin(IntegratorFilterMixin, admin.ModelAdmin):
     list_display = [
         "sortable_str",
         "meta_type",
-        "tags",
+        "get__tags",
     ]
     search_fields = [
         "id",
@@ -1113,6 +1118,11 @@ class WorksTypeAdmin(IntegratorFilterMixin, admin.ModelAdmin):
 
     sortable_str.admin_order_field = "name"
     sortable_str.short_description = _("1.2 Configuration du type")
+
+    def get__tags(self, obj):
+        return list(obj.tags.all())
+
+    get__tags.short_description = _("Mots-clés")
 
 
 class PermitRequestAdmin(admin.ModelAdmin):
