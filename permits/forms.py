81b7369537b7f07d818ef180defd094d4bae1a55
Doran Kayoumi <dorankayoumi@gmail.com>
2022-04-14 15:24:33 +0200

    YC-536 Insure that a department was given or that the document is public

--- permits/forms.py
+++ permits/forms.py
@@ -1392,7 +1392,7 @@ class PermitRequestClassifyForm(forms.ModelForm):
 
 class PermitRequestComplementaryDocumentsForm(forms.ModelForm):
     authorised_departments = forms.ModelMultipleChoiceField(
-        queryset=None, widget=forms.CheckboxSelectMultiple,
+        queryset=None, widget=forms.CheckboxSelectMultiple, required=False,
     )
 
     class Meta:
@@ -1461,6 +1461,15 @@ class PermitRequestComplementaryDocumentsForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super(PermitRequestComplementaryDocumentsForm, self).clean()
+
+        if not self.cleaned_data.get(
+            "authorised_departments"
+        ) and not self.cleaned_data.get("is_public"):
+            raise ValidationError(
+                _(
+                    "Un département doit être renseigner ou le document doit être publique"
+                )
+            )
 
         if not self.cleaned_data.get("document_type"):
             raise ValidationError(_("Un type doit être renseigné!"))
