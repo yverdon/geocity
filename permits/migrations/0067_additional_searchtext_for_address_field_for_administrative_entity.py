e0e27fe3f876767cb111bdbaa5d9017f753588f0
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-05-05 14:03:38 +0200

    rename migrations

--- permits/migrations/0067_additional_searchtext_for_address_field_for_administrative_entity.py
+++ permits/migrations/0067_additional_searchtext_for_address_field_for_administrative_entity.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('permits', '0065_add_geocoding_option_for_adsress_fields_and_adress_search_param'),
+        ('permits', '0066_adapt_admin_texts'),
     ]
 
     operations = [
