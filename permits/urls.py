ed6c40ae691e7d3cb6e2dc833ef67da545852aae
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-06-01 09:31:37 +0200

    get rid of qgisserver for mask layer

--- permits/urls.py
+++ permits/urls.py
@@ -100,16 +100,10 @@ urlpatterns = [
         name="administrative_entities_geojson",
     ),
     path(
-        "adminentitiesmaskgeojson/<int:administrative_entity_id>/",
-        geoviews.administrative_entities_mask_geojson,
-        name="administrative_entities_mask_geojson",
-    ),
-    path(
         "media/<int:property_value_id>/",
         views.permit_request_media_download,
         name="permit_request_media_download",
     ),
-    path("qgisserverproxy/", geoviews.qgisserver_proxy, name="qgisserver_proxy"),
     path("search/", views.permit_requests_search, name="permit_requests_search"),
     path(
         "print-setup/<int:permit_request_id>/<int:print_setup_id>/",
