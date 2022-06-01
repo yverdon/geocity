d795f877839ff4ec9a8d879381463f07aadd43d2
olivierdalang <olivier.dalang@gmail.com>
2022-06-01 10:38:29 +0200

    remove blocks from admin index

--- streamblocks/admin.py
+++ streamblocks/admin.py
@@ -2,9 +2,28 @@ from django.contrib import admin
 from streamfield.admin import StreamBlocksAdmin
 
 from permits.admin import QgisProjectAdminForm
-from .models import PrintBlockMap
+from . import models
 
-admin.site.unregister(PrintBlockMap)
-@admin.register(PrintBlockMap)
-class PrintBlockMapAdmin(StreamBlocksAdmin, admin.ModelAdmin):
+# django-streamfield auto-registers the models, we need to unregister them
+# to customize their admin
+# see: https://github.com/raagin/django-streamfield#custom-admin-class-for-blocks-models
+admin.site.unregister(models.PrintBlockRichText)
+admin.site.unregister(models.PrintBlockCustom)
+admin.site.unregister(models.PrintBlockMap)
+admin.site.unregister(models.PrintBlockContacts)
+admin.site.unregister(models.PrintBlockValidation)
+
+
+@admin.register(models.PrintBlockRichText)
+@admin.register(models.PrintBlockCustom)
+@admin.register(models.PrintBlockContacts)
+@admin.register(models.PrintBlockValidation)
+class PrintBlockAdmin(admin.ModelAdmin):
+    # prevent the model from showing up in the index page
+    def has_module_permission(self, request):
+        return False
+
+@admin.register(models.PrintBlockMap)
+class PrintBlockMapAdmin(StreamBlocksAdmin, PrintBlockAdmin):
+    # use a form that takes care of processing the QGIS project so that it works internally
     form = QgisProjectAdminForm
