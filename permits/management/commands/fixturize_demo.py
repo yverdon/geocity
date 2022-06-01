559032d38f09fb2dcec35064c459daafa8347322
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-06-01 14:17:02 +0200

    fixtures

--- permits/management/commands/fixturize_demo.py
+++ permits/management/commands/fixturize_demo.py
@@ -232,9 +232,6 @@ class Command(BaseCommand):
         self.stdout.write("pilot-2 / demo")
 
         secretary_groups = Group.objects.filter(name__in=["pilot", "pilot-2"])
-        department = models.PermitDepartment.objects.filter(
-            group__in=secretary_groups
-        ).update(is_backoffice=True)
 
         user = self.create_user(
             "validator",
@@ -304,6 +301,12 @@ class Command(BaseCommand):
                 models.PermitWorkflowStatus.objects.get_or_create(
                     status=status_value[0], administrative_entity=entity
                 )
+        # Add admin user in all groups
+        groups = Group.objects.all()
+        user_admin = User.objects.get(username='admin')
+        for group in groups:
+            user_admin.groups.add(group)
+
 
     def create_user(
         self,
@@ -421,9 +424,14 @@ class Command(BaseCommand):
                     ("Demande de macaron", properties["comment"], properties["date"],),
                     (
                         "Accès au centre-ville historique",
+                        properties["plan"],
+                        properties["width"],
                         properties["comment"],
                         properties["title"],
                         properties["date"],
+                        properties["checkbox"],
+                        properties["adresse"],
+                        properties["list_multiple"],
                     ),
                 ],
             ),
@@ -432,26 +440,46 @@ class Command(BaseCommand):
                 [
                     (
                         "Événement sportif",
+                        properties["plan"],
+                        properties["width"],
                         properties["comment"],
                         properties["title"],
+                        properties["date"],
+                        properties["checkbox"],
+                        properties["adresse"],
                         properties["list_multiple"],
                     ),
                     (
                         "Événement culturel",
+                        properties["plan"],
+                        properties["width"],
                         properties["comment"],
                         properties["title"],
+                        properties["date"],
+                        properties["checkbox"],
+                        properties["adresse"],
                         properties["list_multiple"],
                     ),
                     (
                         "Événement politique",
+                        properties["plan"],
+                        properties["width"],
                         properties["comment"],
                         properties["title"],
+                        properties["date"],
+                        properties["checkbox"],
+                        properties["adresse"],
                         properties["list_multiple"],
                     ),
                     (
                         "Événement commercial",
+                        properties["plan"],
+                        properties["width"],
                         properties["comment"],
                         properties["title"],
+                        properties["date"],
+                        properties["checkbox"],
+                        properties["adresse"],
                         properties["list_multiple"],
                     ),
                 ],
@@ -756,6 +784,86 @@ class Command(BaseCommand):
             geom="GEOMETRYCOLLECTION(MULTILINESTRING((2539096.09997796 1181119.41274907,2539094.37477054 1181134.07701214,2539094.37477054 1181134.07701214)), MULTIPOLYGON(((2539102.56950579 1181128.03878617,2539101.27560022 1181139.2526344,2539111.19554289 1181140.11523811,2539111.62684475 1181134.07701214,2539111.62684475 1181134.07701214,2539102.56950579 1181128.03878617))), MULTIPOINT((2539076.69139448 1181128.47008802)))",
         )
 
+        # Permit Request to Classify with mixed objects with lots of text for print demo
+        permit_request7 = models.PermitRequest.objects.create(
+            status=models.PermitRequest.STATUS_PROCESSING,
+            administrative_entity=demo_administrative_entity,
+            author=demo_author,
+            is_public=True,
+        )
+
+        models.PermitRequestValidation.objects.get_or_create(
+            permit_request=permit_request7,
+            department=department,
+            validation_status=models.PermitRequestValidation.STATUS_APPROVED,
+            comment_before=demo_long_text,
+            comment_during=demo_long_text,
+            comment_after=demo_long_text,
+        )
+        models.PermitRequestValidation.objects.get_or_create(
+            permit_request=permit_request7,
+            department=models.PermitDepartment.objects.get(group__name="validator-2"),
+            validation_status=models.PermitRequestValidation.STATUS_APPROVED,
+            comment_before=demo_long_text,
+            comment_during=demo_long_text,
+            comment_after=demo_long_text,
+        )
+
+        models.WorksObjectTypeChoice.objects.create(
+            permit_request=permit_request7, works_object_type=demo_works_object_type
+        )
+
+        models.WorksObjectTypeChoice.objects.create(
+            permit_request=permit_request7,
+            works_object_type=demo_works_object_type_no_validation_document,
+        )
+
+        models.PermitRequestGeoTime.objects.create(
+            permit_request=permit_request7,
+            starts_at=timezone.now(),
+            ends_at=timezone.now(),
+            geom="GEOMETRYCOLLECTION(MULTILINESTRING((2539096.09997796 1181119.41274907,2539094.37477054 1181134.07701214,2539094.37477054 1181134.07701214)), MULTIPOLYGON(((2539102.56950579 1181128.03878617,2539101.27560022 1181139.2526344,2539111.19554289 1181140.11523811,2539111.62684475 1181134.07701214,2539111.62684475 1181134.07701214,2539102.56950579 1181128.03878617))), MULTIPOINT((2539076.69139448 1181128.47008802)))",
+        )
+
+        amend_property_1 = models.PermitRequestAmendProperty.objects.create(
+            name="Commentaire interne",
+            is_visible_by_author=False,
+        )
+        amend_property_1.works_object_types.set([demo_works_object_type, demo_works_object_type_no_validation_document])
+        amend_property_2 = models.PermitRequestAmendProperty.objects.create(
+            name="Commentaire visible par le requérant",
+            is_visible_by_author=True,
+        )
+        amend_property_2.works_object_types.set([demo_works_object_type, demo_works_object_type_no_validation_document])
+        works_object_type_choice_1 = models.WorksObjectTypeChoice.objects.get(
+            permit_request=permit_request7,
+            works_object_type=demo_works_object_type,
+        )
+        works_object_type_choice_2 = models.WorksObjectTypeChoice.objects.get(
+            permit_request=permit_request7,
+            works_object_type=demo_works_object_type_no_validation_document,
+        )
+        models.PermitRequestAmendPropertyValue.objects.create(
+            property=amend_property_1,
+            works_object_type_choice=works_object_type_choice_1,
+            value=demo_long_text,
+        )
+        models.PermitRequestAmendPropertyValue.objects.create(
+            property=amend_property_1,
+            works_object_type_choice=works_object_type_choice_2,
+            value=demo_long_text,
+        )
+        models.PermitRequestAmendPropertyValue.objects.create(
+            property=amend_property_2,
+            works_object_type_choice=works_object_type_choice_1,
+            value=demo_long_text,
+        )
+        models.PermitRequestAmendPropertyValue.objects.create(
+            property=amend_property_2,
+            works_object_type_choice=works_object_type_choice_2,
+            value=demo_long_text,
+        )
+
     def create_geom_layer_entity(self):
 
         models.GeomLayer.objects.create(
@@ -886,3 +994,51 @@ class Command(BaseCommand):
         <p>Consultez les emails générés par l'application:</p>
         => <a href="https://mailhog.geocity.ch" target="_blank">Boîte mail de demo<a/>
         """
+
+"""
+A long text to fille demo permits text fields
+"""
+
+demo_long_text = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+sunt in culpa qui officia deserunt mollit anim id est laborum.
+Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
+quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi 
+tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem 
+ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea 
+voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+1914 translation by H. Rackham
+
+But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a 
+complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. 
+No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally 
+encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it 
+is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, 
+which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a 
+man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?
+Section 1.10.33 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC
+
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos 
+dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt 
+mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, 
+cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, 
+omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae 
+sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur 
+aut perferendis doloribus asperiores repellat.
+1914 translation by H. Rackham
+
+On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure 
+of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to 
+those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases 
+are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being 
+able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims
+of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted.
+The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures,
+or else he endures pains to avoid worse pains.
+"""
