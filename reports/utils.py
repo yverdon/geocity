1ac5fa0cd5b0c23a31363e6838f395fe66404e68
olivierdalang <olivier.dalang@gmail.com>
2022-06-20 14:19:14 +0200

    increase default timeout for container

--- reports/utils.py
+++ reports/utils.py
@@ -29,7 +29,7 @@ def run_docker_container(image, commands, file_input: Tuple[str, IO], file_outpu
 
     # Run the container
     container.start()
-    r = container.wait(timeout=30)
+    r = container.wait(timeout=60)
 
     logs = container.logs().decode("utf-8")
     print(logs)
