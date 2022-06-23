982a8da1791448e7c23689d8251cc837fbf47c2e
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-06-23 07:39:46 +0200

    disable field for integrator

--- permits/admin.py
+++ permits/admin.py
@@ -43,7 +43,6 @@ INTEGRATOR_PERMITS_MODELS_PERMISSIONS = [
     "permitworkflowstatus",
     "permitauthor",
     "qgisproject",
-    "complementarydocumenttype",
 ]
 OTHER_PERMISSIONS_CODENAMES = [
     "view_user",
@@ -866,6 +865,15 @@ class WorksObjectTypeAdmin(IntegratorFilterMixin, admin.ModelAdmin):
             },
         ),
     )
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []
+        else:
+            return [
+                "document_enabled",
+                "publication_enabled",
+            ]
 
     def sortable_str(self, obj):
         return obj.__str__()
