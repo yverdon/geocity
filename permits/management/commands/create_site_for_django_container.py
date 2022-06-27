49c1396bb42cdfcc5900d673166e0e10b78c0029
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-06-27 10:55:29 +0200

    add command to create web site used by print service

---
+++ permits/management/commands/create_site_for_django_container.py
@@ -0,0 +1,21 @@
+from django.contrib.sites.models import Site
+from django.core.management import BaseCommand, CommandError
+from django.utils.translation import gettext
+
+from permits import admin
+
+
+class Command(BaseCommand):
+
+    help = gettext(
+        "Create a site whose name corresponds to the django container 'web'." 
+    )
+
+    def handle(self, *args, **options):
+        self.stdout.write("Creating 'web' site ...")
+
+        try:
+            Site.objects.update_or_create(domain="web", name="web (internal calls)")
+            self.stdout.write("Creation of default 'web' site successful.")
+        except CommandError:
+            self.stdout.write("ERROR: Error while creating 'web' site, print will not work !")
