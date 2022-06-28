508c0c2beec0708053d5529019b152c9fedcc28c
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-06-28 08:39:22 +0200

    remove try/catch

--- reports/management/commands/add_default_report_for_integrator.py
+++ reports/management/commands/add_default_report_for_integrator.py
@@ -12,17 +12,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Creating default print template for integrator ...")
 
-        try:
-            # This could fail if the code has diverged from old migrations because we cannot use
-            # historical models. Since this data migration is not critical, we swallow exceptions,
-            # to avoid breaking migrations down the line.
-            from django.contrib.auth.models import Group
+        # This could fail if the code has diverged from old migrations because we cannot use
+        # historical models. Since this data migration is not critical, we swallow exceptions,
+        # to avoid breaking migrations down the line.
+        from django.contrib.auth.models import Group
 
-            with transaction.atomic():
-                for integrator in Group.objects.all():
-                    create_default_report_for_integrator(Group, integrator, True)
+        with transaction.atomic():
+            for integrator in Group.objects.all():
+                create_default_report_for_integrator(Group, integrator, True)
 
-        except CommandError as e:
-            print(
-                f"Warning: automatic creation of default report for existing integrators failed with exception:\n{e}"
-            )
