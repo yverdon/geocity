5582b6a760644b26f2ea45bd719bf14615b7eb1c
Junod Alexandre <AJU@ylb.ch>
2022-06-29 15:58:14 +0200

    add todo

--- permits/management/commands/create_knox_token.py
+++ permits/management/commands/create_knox_token.py
@@ -22,12 +22,13 @@ class Command(BaseCommand):
         user_id = options["user_id"][0]
         request_user_id = options["user_id"][1]
 
+        # TODO: Add permissions (on model) to create token of another user, if needed
         # Prevent the creation of the knox token on another user than himself
         if user_id and user_id == request_user_id:
             user = User.objects.get(id=user_id)
         else:
             raise CommandError
-
+        # TODO: Define when a token needs to be deleted
         with transaction.atomic():
             try:
                 authtoken, token = AuthToken.objects.create(user, expiry=None)
