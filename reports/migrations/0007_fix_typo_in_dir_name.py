fc4ecb06e132b0630861b5ca2aac10772b5f5e78
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-06-29 18:40:43 +0200

    lint

--- reports/migrations/0007_fix_typo_in_dir_name.py
+++ reports/migrations/0007_fix_typo_in_dir_name.py
@@ -6,13 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reports', '0006_set_background_file_public_and_switch_blocks_names'),
+        ("reports", "0006_set_background_file_public_and_switch_blocks_names"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='reportlayout',
-            name='background',
-            field=models.ImageField(blank=True, help_text='Image d\'arrière plan ("papier à en-tête"). Attention, ces documents ne sont PAS sécurisés', null=True, upload_to='background_paper'),
+            model_name="reportlayout",
+            name="background",
+            field=models.ImageField(
+                blank=True,
+                help_text='Image d\'arrière plan ("papier à en-tête"). Attention, ces documents ne sont PAS sécurisés',
+                null=True,
+                upload_to="background_paper",
+            ),
         ),
     ]
