468bb0d4f19dcedb4e82a1f20a4352df60037eec
olivierdalang <olivier.dalang@gmail.com>
2022-09-01 14:44:35 +0200

    [refactoring][05B] remove dotenv

--- geocity/settings.py
+++ geocity/settings.py
@@ -1,9 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
