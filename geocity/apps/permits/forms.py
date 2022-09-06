8fc2ee7ca0290047bdeab296049dc06538268fa7
Rémi Bovard <remi.bovard@vevey.ch>
2022-09-06 16:18:12 +0200

    Do not add approved and rejected statuses for permit with backoffice field always updatable

--- geocity/apps/permits/forms.py
+++ geocity/apps/permits/forms.py
@@ -991,12 +991,8 @@ class PermitRequestAdditionalInformationForm(forms.ModelForm):
                     self.instance.administrative_entity
                 )
             )
-            # If an amend property in the permit request can always be amended or permit request backoffice fields can always be updated,
-            # STATUS_APPROVED is added to the list
-            if (
-                self.instance.get_amend_property_list_always_amendable()
-                or self.instance.can_always_be_updated(user)
-            ):
+            # If an amend property in the permit request can always be amended, some statuses are added to the list
+            if self.instance.can_always_be_updated(user):
                 filter1 = [
                     tup
                     for tup in models.PermitRequest.STATUS_CHOICES
