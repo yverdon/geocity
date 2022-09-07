8062e54e854ddbd8f0191950eed151e37c2f9822
Rémi Bovard <remi.bovard@vevey.ch>
2022-09-07 11:25:23 +0200

    Do not allow to send an custom email when status changed to received (because email is already sent in this case)

--- geocity/apps/core/static/js/permit_request_amending.js
+++ geocity/apps/core/static/js/permit_request_amending.js
@@ -1,12 +1,21 @@
-const COMPLEMENTARY_INFO = 4
+const STATUS_AWAITING_SUPPLEMENT = 4
+const STATUS_RECEIVED = 7
 const notify = document.getElementById('id_notify_author')
 const statusChange = document.getElementById('id_status')
 const reason = document.getElementById("id_reason")
 
 statusChange.addEventListener('change', (e) => {
-  if (parseInt(e.target.value) === COMPLEMENTARY_INFO) {
+  if (parseInt(e.target.value) === STATUS_AWAITING_SUPPLEMENT) {
     notify.checked = true
     notify.dispatchEvent(new Event("change"))
+  }
+  else if (parseInt(e.target.value) === STATUS_RECEIVED) {
+    notify.checked = false
+    notify.dispatchEvent(new Event("change"))
+    notify.closest('.form-group').style.display = "none"
+  }
+  else {
+    notify.closest('.form-group').style.display = "flex"
   }
 })
 
@@ -16,7 +25,8 @@ notify.addEventListener('change', (e) => {
 
 changeReasonDisplay()
 
-if (parseInt(statusChange.value) === COMPLEMENTARY_INFO) {
+if (parseInt(statusChange.value) === STATUS_AWAITING_SUPPLEMENT ||
+  parseInt(statusChange.value) === STATUS_RECEIVED) {
   statusChange.dispatchEvent(new Event("change"))
 }
 
