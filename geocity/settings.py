f703e4e505174ac00ec14713017e3c993ca32492
olivierdalang <olivier.dalang@gmail.com>
2022-09-07 16:55:00 +0200

    [refactoring][11B] remove unused settings.ARCHIVE_URL
    (seems unused in the code base)

--- geocity/settings.py
+++ geocity/settings.py
@@ -529,7 +529,6 @@ PRIVATE_MEDIA_ROOT = "/private_documents"
 MEDIA_URL = os.environ.get("MEDIA_URL", "/media/")
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 
-ARCHIVE_URL = os.environ.get("ARCHIVE_URL", "/archive/")
 ARCHIVE_ROOT = os.environ.get("ARCHIVE_ROOT", "/archive")
 
 MIN_START_DELAY = os.getenv("MIN_START_DELAY")
