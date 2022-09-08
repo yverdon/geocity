d161ae3e98e72d1edf8909af0efeedf2fc8214fb
Rémi Bovard <remi.bovard@vevey.ch>
2022-09-08 08:11:15 +0200

    Codestyle

--- geocity/apps/core/static/js/permit_request_amending.js
+++ geocity/apps/core/static/js/permit_request_amending.js
@@ -1,10 +1,10 @@
 const STATUS_AWAITING_SUPPLEMENT = 4
 const STATUS_RECEIVED = 7
-const notify = document.getElementById('id_notify_author')
-const statusChange = document.getElementById('id_status')
+const notify = document.getElementById("id_notify_author")
+const statusChange = document.getElementById("id_status")
 const reason = document.getElementById("id_reason")
 
-statusChange.addEventListener('change', (e) => {
+statusChange.addEventListener("change", (e) => {
   if (parseInt(e.target.value) === STATUS_AWAITING_SUPPLEMENT) {
     notify.checked = true
     notify.dispatchEvent(new Event("change"))
@@ -12,14 +12,14 @@ statusChange.addEventListener('change', (e) => {
   else if (parseInt(e.target.value) === STATUS_RECEIVED) {
     notify.checked = false
     notify.dispatchEvent(new Event("change"))
-    notify.closest('.form-group').style.display = "none"
+    notify.closest(".form-group").style.display = "none"
   }
   else {
-    notify.closest('.form-group').style.display = "flex"
+    notify.closest(".form-group").style.display = "flex"
   }
 })
 
-notify.addEventListener('change', (e) => {
+notify.addEventListener("change", (e) => {
   changeReasonDisplay(e.target.checked)
 })
 
@@ -31,5 +31,5 @@ if (parseInt(statusChange.value) === STATUS_AWAITING_SUPPLEMENT ||
 }
 
 function changeReasonDisplay(display = false) {
-  reason.closest('.form-group').style.display = display ? 'flex' : 'none'
+  reason.closest(".form-group").style.display = display ? "flex" : "none"
 }
