d7b8829c557b978f8465e7aeace76749be47147b
AlexandreJunod <alexandre.junod@hotmail.com>
2022-10-07 14:02:22 +0200

    re-order migrations based on what is already on main

--- geocity/apps/permits/migrations/0085_add_shortname.py
+++ geocity/apps/permits/migrations/0085_add_shortname.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("permits", "0084_archivedpermitrequest_archive"),
+        ("permits", "0084_change_text"),
     ]
 
     operations = [
