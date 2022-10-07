b65d7fdbf2bfd6accea19267e2200118a2dcdb82
AlexandreJunod <alexandre.junod@hotmail.com>
2022-10-07 14:00:57 +0200

    re-order migrations

--- geocity/apps/permits/migrations/0085_add_shortname.py
+++ geocity/apps/permits/migrations/0085_add_shortname.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("permits", "0082_change_text"),
+        ("permits", "0084_archivedpermitrequest_archive"),
     ]
 
     operations = [
