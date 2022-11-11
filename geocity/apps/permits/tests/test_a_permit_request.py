143ca0217a1992f14372c2114267be11ee25d0b5
Sylvain Fankhauser <sephi@fhtagn.top>
2022-11-11 13:16:38 +0100

    Fix SubmissionFilteredFormListTestCase

--- geocity/apps/permits/tests/test_a_permit_request.py
+++ geocity/apps/permits/tests/test_a_permit_request.py
@@ -2403,59 +2403,6 @@ class AdministrativeEntitySecretaryEmailTestcase(TestCase):
         )
 
 
-class SubmissionFilteredFormListTestCase(LoggedInSecretariatMixin, TestCase):
-    def setUp(self):
-        super().setUp()
-        self.form_normal = factories.FormFactory()
-        self.submission = factories.SubmissionFactory(
-            author=self.user,
-            status=submissions_models.Submission.STATUS_APPROVED,
-        )
-        self.submission.forms.set([self.form_normal])
-        factories.SubmissionGeoTimeFactory(submission=self.submission)
-
-        selected_form = self.submission.selectedform_set.first()
-        field = factories.FieldFactory()
-        field.forms.add(selected_form.form)
-        self.field_value = factories.FieldValueFactory(
-            selected_form=selected_form, field=field
-        )
-
-    def test_secretariat_user_can_see_filtered_submission_details(
-        self,
-    ):
-        response = self.client.get(
-            "{}?forms__form={}".format(
-                reverse(
-                    "submissions:submissions_list",
-                ),
-                self.submission.forms.first().form.id,
-            )
-        )
-
-        self.assertInHTML(self.field_value.value["val"], response.content.decode())
-
-    def test_secretariat_user_can_see_filtered_submission_details_in_xlsx(
-        self,
-    ):
-        response = self.client.get(
-            "{}?forms__form={}&_export=xlsx".format(
-                reverse(
-                    "submissions:submissions_list",
-                ),
-                self.submission.forms.first().form.id,
-            )
-        )
-
-        content = io.BytesIO(response.content)
-
-        # Replace content in bytes with the readable one
-        response.content = tablib.import_set(content.read(), format="xlsx")
-
-        self.assertContains(response, self.field_value.value["val"])
-        self.assertContains(response, self.field_value.field)
-
-
 class SubmissionAnonymousTestCase(TestCase):
     def setUp(self):
         super().setUp()
