9dd3dfa536cc7a8797726405126b7daafec70856
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-11-17 11:22:32 +0100

    remove unused file

--- geocity/admin_site.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.admin import AdminSite, site
-
-
-# TODO: delete ?
-class PermitsAdminSite(AdminSite):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._registry.update(site._registry)
