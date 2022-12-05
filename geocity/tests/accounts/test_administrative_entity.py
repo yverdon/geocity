3d0652fea684ec40add7def95b2e6a8752f53846
Cornut Matthieu <matthieu.cornut@liip.ch>
2022-12-05 12:58:01 +0100

    YC-774 Adapt administrative entity validation

--- geocity/tests/accounts/test_administrative_entity.py
+++ geocity/tests/accounts/test_administrative_entity.py
@@ -6,17 +6,17 @@ from geocity.tests.utils import LoggedInUserMixin
 
 
 class AdministrativeEntityTestCase(LoggedInUserMixin, TestCase):
-    def test_cant_save_single_demands_by_form_if_multi_forms_exist(self):
+    def test_administrative_entity_containing_shared_public_forms_is_invalid(self):
         entity1 = AdministrativeEntityFactory()
         entity2 = AdministrativeEntityFactory()
         FormFactory(administrative_entities=[entity1, entity2], is_public=True)
-
-        entity1.is_single_form_submissions = True
-        self.assertRaises(ValidationError, entity1.clean)
-
-    def test_can_save_single_demands_by_form(self):
-        entity1 = AdministrativeEntityFactory()
         FormFactory(administrative_entities=[entity1], is_public=True)
 
-        entity1.is_single_form_submissions = True
+        self.assertRaises(ValidationError, entity1.clean)
+
+    def test_administrative_entity_containing_owned_public_forms_is_valid(self):
+        entity1 = AdministrativeEntityFactory()
+        FormFactory(administrative_entities=[entity1], is_public=True)
+        FormFactory(administrative_entities=[entity1], is_public=True)
+
         entity1.clean()
