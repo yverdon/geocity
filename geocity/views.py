388c83c97e99e7aaee1d7609493f0182a56111e1
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2022-12-06 09:40:07 +0100

    support suffix urls

--- geocity/views.py
+++ geocity/views.py
@@ -1,8 +1,16 @@
+from django.conf import settings
 from django.shortcuts import redirect
 
 
 # Redirect "permit-requests" URLs to "submissions" URLs. Will be deprecated in 3.0
 def legacy_urls_redirect(request):
 
-    new_url = f'/submissions{request.path.split("permit-requests")[1]}?{request.META["QUERY_STRING"]}'
+    new_url = f'/submissions{request.path.split("permit-requests")[1]}'
+
+    if settings.PREFIX_URL:
+        new_url = f"{settings.PREFIX_URL}{new_url}"
+
+    if request.META["QUERY_STRING"]:
+        new_url = f'{new_url}?{request.META["QUERY_STRING"]}'
+
     return redirect(new_url)
