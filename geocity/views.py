d711b3a4a1b15f15cd1a13739a4fac1ed5135d2d
Rémi Bovard <remi.bovard@vevey.ch>
2022-12-06 09:49:54 +0100

    Add leading slash before PREFIX_URL

--- geocity/views.py
+++ geocity/views.py
@@ -5,12 +5,14 @@ from django.shortcuts import redirect
 # Redirect "permit-requests" URLs to "submissions" URLs. Will be deprecated in 3.0
 def legacy_urls_redirect(request):
 
-    new_url = f'/submissions{request.path.split("permit-requests")[1]}'
+    new_url = f'submissions{request.path.split("permit-requests")[1]}'
 
     if settings.PREFIX_URL:
         new_url = f"{settings.PREFIX_URL}{new_url}"
 
     if request.META["QUERY_STRING"]:
         new_url = f'{new_url}?{request.META["QUERY_STRING"]}'
+
+    new_url = f"/{new_url}"
 
     return redirect(new_url)
