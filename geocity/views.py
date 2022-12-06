c55b0afb461f9376e82e4148c6dd62397264a1d6
Rémi Bovard <remi.bovard@vevey.ch>
2022-12-06 16:31:35 +0100

    Refactoring

--- geocity/views.py
+++ geocity/views.py
@@ -1,18 +1,11 @@
-from django.conf import settings
 from django.shortcuts import redirect
 
 
 # Redirect "permit-requests" URLs to "submissions" URLs. Will be deprecated in 3.0
 def legacy_urls_redirect(request):
-
-    new_url = f'submissions{request.path.split("permit-requests")[1]}'
-
-    if settings.PREFIX_URL:
-        new_url = f"{settings.PREFIX_URL}{new_url}"
+    new_url = request.path.replace("permit-requests/", "submissions/")
 
     if request.META["QUERY_STRING"]:
         new_url = f'{new_url}?{request.META["QUERY_STRING"]}'
-
-    new_url = f"/{new_url}"
 
     return redirect(new_url)
