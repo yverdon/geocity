160646c9d19b47015b53919e4704a2ab9b9eac53
AlexandreJunod <alexandre.junod@hotmail.com>
2023-01-09 15:50:24 +0100

    fix migration conflicts

--- geocity/apps/submissions/migrations/0013_alter_complementarydocumenttype_integrator_and_more.py
+++ geocity/apps/submissions/migrations/0013_alter_complementarydocumenttype_integrator_and_more.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("auth", "0012_alter_user_first_name_max_length"),
-        ("submissions", "0009_alter_historicalcontact_options_and_more"),
+        ("submissions", "0012_alter_postfinancetransaction_options"),
     ]
 
     operations = [
