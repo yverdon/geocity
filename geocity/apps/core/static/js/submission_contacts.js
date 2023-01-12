1a8f7020f117a5058cc68b5113102eec7f029610
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2023-01-12 10:14:41 +0100

    set fields as readonly when copied

--- geocity/apps/core/static/js/submission_contacts.js
+++ geocity/apps/core/static/js/submission_contacts.js
@@ -4,6 +4,7 @@ let update_form_value = function(item, userprofile) {
       document.getElementById(`id_form-${item}-first_name`).value = userprofile.first_name;
       for (const [key, value] of Object.entries(userprofile)) {
         document.getElementById(`id_form-${item}-${key}`).value = value;
+        document.getElementById(`id_form-${item}-${key}`).readOnly = true;
       }
     } else {
       for (const [key, value] of Object.entries(userprofile)) {
