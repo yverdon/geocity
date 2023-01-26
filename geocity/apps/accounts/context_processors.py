45c0f128065d3c12e9020fbdb026a8e406b65696
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2023-01-26 17:37:42 +0100

    one migration only

--- geocity/apps/accounts/context_processors.py
+++ geocity/apps/accounts/context_processors.py
@@ -1,34 +1,45 @@
 from constance import config
-from .models import TemplateCustomization
 from django.contrib.sites.shortcuts import get_current_site
 
 
 def site_customization(request):
     site = get_current_site(request)
     template = site.site_profile.custom_template
-    print(dir(config))
-    print(config)
 
     if template:
-        customization =  {
-            "PRIMARY_COLOR": template.primary_color if template.primary_color else config.PRIMARY_COLOR,
-            "SECONDARY_COLOR": template.secondary_color if template.secondary_color else config.SECONDARY_COLOR,
-            "BACKGROUND_COLOR": template.primary_color if template.primary_color else config.BACKGROUND_COLOR,
-            "LOGIN_BACKGROUND_COLOR": template.primary_color if template.primary_color else config.LOGIN_BACKGROUND_COLOR,
-            "TEXT_COLOR": template.primary_color if template.primary_color else config.TEXT_COLOR,
-            "TITLE_COLOR": template.primary_color if template.primary_color else config.TITLE_COLOR,
-            "TABLE_COLOR": template.primary_color if template.primary_color else config.TABLE_COLOR,
+        customization = {
+            "PRIMARY_COLOR": template.primary_color
+            if template.primary_color
+            else config.PRIMARY_COLOR,
+            "SECONDARY_COLOR": template.secondary_color
+            if template.secondary_color
+            else config.SECONDARY_COLOR,
+            "BACKGROUND_COLOR": template.primary_color
+            if template.primary_color
+            else config.BACKGROUND_COLOR,
+            "LOGIN_BACKGROUND_COLOR": template.primary_color
+            if template.primary_color
+            else config.LOGIN_BACKGROUND_COLOR,
+            "TEXT_COLOR": template.primary_color
+            if template.primary_color
+            else config.TEXT_COLOR,
+            "TITLE_COLOR": template.primary_color
+            if template.primary_color
+            else config.TITLE_COLOR,
+            "TABLE_COLOR": template.primary_color
+            if template.primary_color
+            else config.TABLE_COLOR,
         }
         return customization
 
-    customization =  {
-            "PRIMARY_COLOR": config.PRIMARY_COLOR,
-            "SECONDARY_COLOR": config.SECONDARY_COLOR,
-            "BACKGROUND_COLOR": config.BACKGROUND_COLOR,
-            "LOGIN_BACKGROUND_COLOR": config.LOGIN_BACKGROUND_COLOR,
-            "TEXT_COLOR": config.TEXT_COLOR,
-            "TITLE_COLOR": config.TITLE_COLOR,
-            "TABLE_COLOR": config.TABLE_COLOR,
-        }
+    customization = {
+        "PRIMARY_COLOR": config.PRIMARY_COLOR,
+        "SECONDARY_COLOR": config.SECONDARY_COLOR,
+        "BACKGROUND_COLOR": config.BACKGROUND_COLOR,
+        "LOGIN_BACKGROUND_COLOR": config.LOGIN_BACKGROUND_COLOR,
+        "TEXT_COLOR": config.TEXT_COLOR,
+        "TITLE_COLOR": config.TITLE_COLOR,
+        "TABLE_COLOR": config.TABLE_COLOR,
+    }
 
     return customization
