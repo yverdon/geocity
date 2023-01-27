2c7b01739f9f1d41df3f5cbae7fca14b1c101a7c
AlexandreJunod <alexandre.junod@hotmail.com>
2023-01-27 10:56:39 +0100

    adapt html to use buttons instead of a href and toggle disable on button

---
+++ geocity/apps/core/static/js/submission_submit.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("btn_accept_terms");
+const checkbox = document.getElementById("chk_accept_terms");
+const message = document.getElementByClassName("maximum-submission-message");
+
+checkbox.addEventListener("click", () => {
+    // Toggle the disable attribute based on message visible or not
+    if (!message) { 
+        button.toggleAttribute("disabled");
+    }
+});
