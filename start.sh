54aac0112aade969a315e1f30f8905eb441f548c
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2023-03-14 23:31:32 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 echo "Restarting the application..."
 
 docker compose down --remove-orphans \
-&& docker compose up --build -d \
+&& docker compose build --progress plain \
+&& docker compose up -d \
 && docker compose logs --tail 64 -tf web
 
