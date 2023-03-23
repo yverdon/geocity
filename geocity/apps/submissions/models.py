692374b770c5b528cd37c0fa859d0070625346fd
AlexandreJunod <alexandre.junod@hotmail.com>
2023-03-23 13:38:17 +0100

    replace editer by modifier

--- geocity/apps/submissions/models.py
+++ geocity/apps/submissions/models.py
@@ -271,10 +271,10 @@ class Submission(models.Model):
         permissions = [
             ("read_submission", _("Consulter les demandes")),
             ("amend_submission", _("Traiter les demandes")),
-            ("edit_submission_validations", _("Éditer les validations")),
+            ("edit_submission_validations", _("Modifier les validations")),
             ("validate_submission", _("Valider les demandes")),
             ("classify_submission", _("Classer les demandes")),
-            ("edit_submission", _("Éditer les demandes")),
+            ("edit_submission", _("Modifier les demandes")),
             ("view_private_form", _("Voir les demandes restreintes")),
             ("can_refund_transactions", _("Rembourser une transaction")),
             ("can_revert_refund_transactions", _("Revenir sur un remboursement")),
