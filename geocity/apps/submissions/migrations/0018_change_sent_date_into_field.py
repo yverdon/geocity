908d3dc66a6e5a6a0137d5a8b39d8fe0cf99a019
monodo <olivier.monod@yverdon-les-bains.ch>
2023-06-07 08:06:47 +0200

    wip - solve data migration issue

--- geocity/apps/submissions/migrations/0018_change_sent_date_into_field.py
+++ geocity/apps/submissions/migrations/0018_change_sent_date_into_field.py
@@ -3,23 +3,6 @@
 from django.db import migrations, models
 
 
-def set_sent_date_from_simple_history(apps, schema_editor):
-    Submission = apps.get_model("submissions", "Submission")
-    for row in Submission.objects.all():
-
-        last_sent_history = (
-            row.history.filter(status=Submission.STATUS_SUBMITTED_FOR_VALIDATION)
-            .order_by("history_date")
-            .last()
-        )
-        last_sent_date_value = (
-            last_sent_history.history_date if last_sent_history is not None else None
-        )
-
-        row.sent_date = last_sent_date_value
-        row.save(update_fields=["sent_date"])
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -37,5 +20,4 @@ class Migration(migrations.Migration):
             name="sent_date",
             field=models.DateTimeField(null=True, verbose_name="date du dernier envoi"),
         ),
-        migrations.RunPython(set_sent_date_from_simple_history),
     ]
