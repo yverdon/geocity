217dee2ae958b5cdef8c781adcd3398e1338a56c
monodo <olivier.monod@yverdon-les-bains.ch>
2023-11-02 14:39:56 +0100

    thumbor service

--- geocity/apps/agenda/urls.py
+++ geocity/apps/agenda/urls.py
@@ -6,15 +6,17 @@ app_name = "agenda"
 
 agenda_urlpatterns = [
     path("", views.agenda, name="agenda"),
+    # Without thumbor params
+    path(
+        "image/display/permit_requests_uploads/<int:form_id>/<str:image_name>",
+        views.image_service,
+        name="image_service",
+    ),
+    # With thumbor params
     path(
         "image/display/permit_requests_uploads/<str:thumbor_params>/<int:form_id>/<str:image_name>",
         views.image_display,
         name="image_display",
-    ),
-    path(
-        "image/service/permit_requests_uploads/<int:form_id>/<str:image_name>",
-        views.image_service,
-        name="image_service",
     ),
 ]
 
--- geocity/apps/agenda/views.py
+++ geocity/apps/agenda/views.py
@@ -1,23 +1,22 @@
-import os, requests
+import os
 
+import requests
+from constance import config
 from django.conf import settings
 from django.http import FileResponse, JsonResponse
 from django.shortcuts import render
-from constance import config
 
 
 def image_display(request, form_id, image_name, thumbor_params):
 
-    if config.THUMBOR_SERVICE_URL:
-        # TODO: use reverse to get the image URL and setup communication between geocity and thumbor composition
-        test_url = 'https%3A%2F%2Fgithub.com%2Fthumbor%2Fthumbor%2Fraw%2Fmaster%2Fexample.jpg'
-        resp = requests.get(f'{config.THUMBOR_SERVICE_URL}/unsafe/{thumbor_params}/{test_url}')
-        
-        thumbor_response =  FileResponse(resp, content_type="image/jpeg")
-        return thumbor_response
-    else:
-        # thumbor service not configured
-        return image_service(request, form_id, image_name)
+    INTERNAL_WEB_ROOT_URL = "http://web:9000"
+    image_url = f"{INTERNAL_WEB_ROOT_URL}/agenda/image/display/permit_requests_uploads/{form_id}/{image_name}"
+    resp = requests.get(
+        f"{config.THUMBOR_SERVICE_URL}/unsafe/{thumbor_params}/{image_url}"
+    )
+    thumbor_response = FileResponse(resp, content_type="image/jpeg")
+    return thumbor_response
+
 
 def image_service(request, form_id, image_name):
 
