3ca7c9d6099ec7cee106ca04430a207919ae2f91
monodo <olivier.monod@yverdon-les-bains.ch>
2023-11-21 15:28:21 +0100

    add agenda-embed to external statics

--- geocity/settings.py
+++ geocity/settings.py
@@ -559,7 +559,7 @@ LANGUAGES = (
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 if DEBUG:
     STATICFILES_DIRS = [
-        "/static_map/",
+        "/external_statics/",
     ]
 
 STATIC_URL = os.environ["STATIC_URL"]
