dedf3a8e1f69ee0e9785c5c785e727dec5e35543
AlexandreJunod <alexandre.junod@hotmail.com>
2023-12-14 08:58:43 +0100

    update libs and fix allauth

--- geocity/settings.py
+++ geocity/settings.py
@@ -207,6 +207,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "axes.middleware.AxesMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 if ENABLE_2FA:
