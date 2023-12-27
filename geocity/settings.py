6bb6211491941b49c424fdcfc8c108d710f0d8e5
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2023-12-27 22:59:36 +0100

    Update logging capabilities.

--- geocity/settings.py
+++ geocity/settings.py
@@ -678,16 +678,26 @@ DEBUG_TOOLBAR_CONFIG = {
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname}: {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname}: {message}",
+            "style": "{",
+        },
+    },
     "handlers": {
         "console": {
             "level": "INFO",
             "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
     },
     "loggers": {
         "": {
             "handlers": ["console"],
-            "level": "ERROR",
             "propagate": True,
         },
     },
