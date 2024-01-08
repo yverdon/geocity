664aa3fd0fa3696933da095a1bc3bdaedf478c30
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2024-01-08 14:03:12 +0100

    Minor fix.

--- geocity/apps/submissions/shortcuts.py
+++ geocity/apps/submissions/shortcuts.py
@@ -24,9 +24,9 @@ def get_submission_for_user_or_404(user, submission_id, statuses=None):
 
 def get_service_fee_for_user_or_404(user, service_fee_id, statuses=None):
     """
-    Return the submission with `submission_id` or raise an Http404 if there is no such submission. The
-    submission must either belong to the given user, or the given user should be in the same administrative entity.
-    If `statuses` is set and a submission is found but its status doesn't match any value in `statuses`,
+    Return the service_fee with `service_fee_id` or raise an Http404 if there is no such service_fee. The
+    service_fee must either belong to the given user, or the given user should be in the same administrative entity.
+    If `statuses` is set and a service_fee is found but its status doesn't match any value in `statuses`,
     `BadSubmissionStatus` will be raised.
     """
     service_fee = get_object_or_404(ServicesFees.objects.filter(pk=service_fee_id))
