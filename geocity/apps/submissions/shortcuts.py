03ad5294719c9471efcca82c86bb47ebc2fe3bb7
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2024-01-12 22:33:21 +0100

    Make get_service_fee_for_user_or_404 function more robust.

--- geocity/apps/submissions/shortcuts.py
+++ geocity/apps/submissions/shortcuts.py
@@ -22,13 +22,15 @@ def get_submission_for_user_or_404(user, submission_id, statuses=None):
     return permit_request
 
 
-def get_service_fee_for_user_or_404(user, service_fee_id, statuses=None):
+def get_service_fee_for_user_or_404(user, service_fee_id=None, statuses=None):
     """
     Return the service_fee with `service_fee_id` or raise an Http404 if there is no such service_fee. The
     service_fee must either belong to the given user, or the given user should be in the same administrative entity.
     If `statuses` is set and a service_fee is found but its status doesn't match any value in `statuses`,
     `BadSubmissionStatus` will be raised.
     """
-    service_fee = get_object_or_404(ServicesFees.objects.filter(pk=service_fee_id))
-
-    return service_fee
+    return (
+        get_object_or_404(ServicesFees.objects.filter(pk=service_fee_id))
+        if service_fee_id
+        else None
+    )
