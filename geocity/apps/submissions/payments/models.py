9f386b7cd9fbc259543cf4546c3c406b2d538abe
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2024-01-17 08:34:06 +0100

    Use a global settings for the currency of service fees.

--- geocity/apps/submissions/payments/models.py
+++ geocity/apps/submissions/payments/models.py
@@ -157,8 +157,8 @@ class ServicesFeesType(models.Model):
         null=True,  # For hourly service fees, this field has to be null
         decimal_places=2,
         max_digits=12,
-        default_currency=settings.DEFAULT_CURRENCY,
-        verbose_name=_("Tarif forfaitaire [CHF]."),
+        default_currency="settings.DEFAULT_CURRENCY",
+        verbose_name=_("Tarif forfaitaire [CHF]"),
         help_text=_("Le tarif forfaitaire de cette prestation."),
     )
     is_visible_by_validator = models.BooleanField(
@@ -278,9 +278,9 @@ class ServicesFees(models.Model):
         default_currency=settings.DEFAULT_CURRENCY,
         verbose_name=_("Montant [CHF]"),
         help_text=_(
-            """Le montant de la prestation.
-            Calulé automatiquement en fonction du tarif horaire.
-            Est fixe si la prestation est forfaitaire."""
+            "Le montant de la prestation. "
+            "Calulé automatiquement en fonction du tarif horaire. "
+            "Est fixe si la prestation est forfaitaire. "
         ),
     )
 
