a5dccdcde73583a24666b506c761bd4ab67c1b10
Rémi Bovard <remi.bovard@vevey.ch>
2024-02-01 16:49:13 +0100

    Add a new env variable to disable migration script at prod startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ set -e
 # If not will, the container will fail and restart.
 python3 manage.py shell -c "import django; django.db.connection.ensure_connection();"
 
-# On PROD, we run migrations on startup.
-if [ "$ENV" == "PROD" ]; then
+# On PROD, we run migrations at startup unless explicitly disabled.
+if [ "$ENV" == "PROD" ] && [ "${DISABLE_MIGRATION_SCRIPT_ON_PRODUCTION}" != "true" ]; then
     scripts/migrate.sh
 fi
 
