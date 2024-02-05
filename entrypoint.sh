8fa3e55bd11217828a9791047d5b9eed8dfe58c8
monodo <olivier.monod@yverdon-les-bains.ch>
2024-02-05 09:13:47 +0100

    run collect static in anycase

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,17 @@ set -e
 # If not will, the container will fail and restart.
 python3 manage.py shell -c "import django; django.db.connection.ensure_connection();"
 
-# On PROD, we run migrations at startup unless explicitly disabled.
-if [ "$ENV" == "PROD" ] && [ "${DISABLE_MIGRATION_SCRIPT_ON_PRODUCTION}" != "true" ]; then
-    scripts/migrate.sh
+# On PROD, we run migrations at startup unless explicitly disabled. If disabled,
+# this commands must be run manually
+if [ "$ENV" == "PROD" ] && [ "${DISABLE_MIGRATION_SCRIPT_ON_PRODUCTION}" != "false" ]; then
+    python3 manage.py migrate
+    python3 manage.py update_integrator_permissions
+fi
+
+# On PROD we always collect statics
+if [ "$ENV" == "PROD" ]; then
+    python3 manage.py compilemessages -l fr
+    python3 manage.py collectstatic --no-input
 fi
 
 # Run the command
