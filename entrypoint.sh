42a6df5a90468b345be0d00725dd02639ddb1371
monodo <olivier.monod@yverdon-les-bains.ch>
2024-02-28 15:31:38 +0100

    use elif

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,12 @@ if [ "$ENV" == "PROD" ] && [ "${DISABLE_MIGRATION_SCRIPT_ON_PRODUCTION}" != "tru
     python3 manage.py migrate
 fi
 
-# On PROD, we always collect statics
 if [ "$ENV" == "PROD" ]; then
     python3 manage.py collectstatic --no-input
     python3 manage.py compilemessages -l fr
     python3 manage.py update_integrator_permissions
 elif [ "$ENV" == "DEV" ]; then
+    python3 manage.py compilemessages -l fr
     python3 manage.py migrate
     python3 manage.py update_integrator_permissions
 fi
